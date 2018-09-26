aa17bb2581de2ea609bcb48bf41af5cb642b0b38
Kwun Yeung <kwun@forbole.com>
2018-09-26 13:58:34 +0800

    move setState out of render()

--- imports/ui/blocks/List.jsx
+++ imports/ui/blocks/List.jsx
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import { Meteor } from 'meteor/meteor';
 // another small block render
 import Block from './block.js';
-// import { Button } from 'reactstrap';
-
-import { Blockscon } from '/imports/api/blocks/blocks.js';
-import { withTracker } from 'meteor/react-meteor-data';
-// import { Server } from 'https';
-
-class Blocks extends Component {
+export default class Blocks extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            blocks: ""
+        }
     }
 
-    updateBlock = () => {
-        Meteor.call('blocksUpdate', (error, result) => {
-            console.log(result);
-        })
+    componentDidUpdate(prevProps, prevState){
+        if (this.props.blocks != prevProps.blocks){
+            let blocks = this.props.blocks.map((block) => (<Block key={block.height} hash={block.hash} block={block}/>));
+            this.setState(
+                {blocks: blocks}
+            )        
+        }
     }
 
     render() {
@@ -28,36 +27,10 @@ class Blocks extends Component {
             )
         }
         else if (this.props.blocks.length > 0) {
-            return this.props.blocks.map((block) => (
-                <Block key={block.height} hash={block.hash} block={block}/>
-            ));
+            return this.state.blocks;
         }
         else{
             return <tr><td>No blocks.</td></tr>
         }
-        // } else {
-        //     return(
-        //         // <Button className="btn btn-primary" onClick={this.updateBlock}>Click me</Button>
-        //     );
-        // }
     }
 }
-
-
-export default BlocksContainer = withTracker((curr) => {
-    const heightHandle = Meteor.subscribe('blocks.height');
-    const loading = !heightHandle.ready();
-    // console.log(loading)
-    // console.log(curr.limit)
-    // console.log(Blocks.getLimit)
-    // const blocks = Blockscon.find({}).fetch();
-    const blocks = Blockscon.find({}, {sort: {height:-1}, limit: curr.limit}).fetch();
-    const blocksExist = !loading && !!blocks;
-    // console.log(props.state.limit);
-    return {
-        loading,
-        blocksExist,
-        blocks: blocksExist ? blocks : {}
-        // blocks: Blockscon.find({}, {sort: {height:-1}, limit: 10}).fetch(),
-    };
-})(Blocks);
