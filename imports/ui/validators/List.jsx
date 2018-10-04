021cb1cdc43b0ca08d6a057ba1d48085abcc4446
Kwun Yeung <kwun@forbole.com>
2018-10-04 23:34:16 +0800

    updated validator list styles

--- imports/ui/validators/List.jsx
+++ imports/ui/validators/List.jsx
@@ -6,7 +6,7 @@ import { Meteor } from 'meteor/meteor';
 
 const ValidatorRow = (props) => {
     let moniker = (props.validator.description.moniker)?props.validator.description.moniker:props.validator.address;
-    return <tr><th scope="row" className="d-none d-sm-block">{props.index+1}</th><td><Link to="#">{moniker}</Link></td><td>{props.validator.voting_power}</td><td><Progress animated value={props.validator.uptimePercent()}>{props.validator.uptimePercent()}%</Progress></td><td>{(props.validator.lastSeen)?moment.utc(props.validator.lastSeen).format("D MMM YYYY, h:mm:ssa z"):''}</td></tr>
+    return <tr><th scope="row" className="d-none d-sm-block counter">{props.index+1}</th><td><Link to="#">{moniker}</Link></td><td>{props.validator.voting_power}</td><td className="uptime"><Progress animated value={props.validator.uptimePercent()}>{props.validator.uptimePercent()}%</Progress></td><td>{(props.validator.lastSeen)?moment.utc(props.validator.lastSeen).format("D MMM YYYY, h:mm:ssa z"):''}</td></tr>
 }
 
 export default class List extends Component{
@@ -38,11 +38,11 @@ export default class List extends Component{
                 <Table striped className="validator-list">
                     <thead>
                         <tr>
-                            <th className="d-none d-sm-block">&nbsp;</th>
-                            <th>Moniker</th>
-                            <th>Voting Power</th>
-                            <th>Uptime (last {Meteor.settings.public.uptimeWindow} blocks}</th>
-                            <th>Last Seen</th>
+                            <th className="d-none d-sm-block counter">&nbsp;</th>
+                            <th className="moniker"><i className="material-icons">perm_contact_calendar</i> <span className="d-none d-sm-inline">Moniker</span></th>
+                            <th className="voting-power"><i className="material-icons">power</i> <span className="d-none d-sm-inline">Voting Power</span></th>
+                            <th className="uptime"><i className="material-icons">thumb_up</i> <span className="d-none d-sm-inline">Uptime (last {Meteor.settings.public.uptimeWindow} blocks)</span></th>
+                            <th className="last-seen"><i className="material-icons">access_time</i> <span className="d-none d-sm-inline">Last Seen</span></th>
                         </tr>
                     </thead>
                     <tbody>{this.state.validators}</tbody>
