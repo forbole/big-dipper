3603003e0425cf7d5b9fba035a46e8b9d57f54bb
Kwun Yeung <kwun@forbole.com>
2018-10-05 01:18:38 +0800

    set data to empty object for chart

--- imports/ui/home/Chart.jsx
+++ imports/ui/home/Chart.jsx
@@ -8,34 +8,10 @@ export default class Chart extends Component{
     constructor(props){
         super(props);
         this.state = {
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [
-                  {
-                    label: 'My First dataset',
-                    fill: false,
-                    lineTension: 0.1,
-                    backgroundColor: 'rgba(75,192,192,0.4)',
-                    borderColor: 'rgba(75,192,192,1)',
-                    borderCapStyle: 'butt',
-                    borderDash: [],
-                    borderDashOffset: 0.0,
-                    borderJoinStyle: 'miter',
-                    pointBorderColor: 'rgba(75,192,192,1)',
-                    pointBackgroundColor: '#fff',
-                    pointBorderWidth: 1,
-                    pointHoverRadius: 5,
-                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                    pointHoverBorderColor: 'rgba(220,220,220,1)',
-                    pointHoverBorderWidth: 2,
-                    pointRadius: 1,
-                    pointHitRadius: 10,
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                  }
-                ]
-              },
-              optionsTime: {},
-              optionsVP: {}
+            vpData: {},
+            timeData: {},
+            optionsTime: {},
+            optionsVP: {}
         }
     }
 
