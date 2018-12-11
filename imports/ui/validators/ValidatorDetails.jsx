d1e551b9ba3ad7ea5e27de576a4b9de35b728d31
Kwun Yeung <kwun@forbole.com>
2018-12-12 03:40:36 +0800

    remove comments in validator details

--- imports/ui/validators/ValidatorDetails.jsx
+++ imports/ui/validators/ValidatorDetails.jsx
@@ -8,14 +8,11 @@ export default class ValidatorDetails extends Component{
     }
 
     render() {
-        console.log(this.props);
         return <div>
         <h1>Validator Details <Badge color="primary">{Meteor.settings.public.chainId}</Badge></h1>
-        {/* <p className="lead">Here is a list of active validators.</p> */}
             <Row>
                 <Col md={12}>
                     <Validator address={this.props.match.params.address} />
-                    {/* <List revoked={false}/> */}
                 </Col>
             </Row>
         </div>
