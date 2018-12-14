20ba6a72f2da9845929ec9c441f9a4b6870d7063
Kwun Yeung <kwun@forbole.com>
2018-12-14 17:38:19 +0800

    add validator status on list

--- imports/ui/validators/List.jsx
+++ imports/ui/validators/List.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table, Progress } from 'reactstrap';
+import { Badge, Progress } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { Meteor } from 'meteor/meteor';
@@ -13,6 +13,7 @@ const ValidatorRow = (props) => {
         <th scope="row" className="d-none d-md-table-cell counter">{props.index+1}</th>
         <td><Link to={"/validator/"+props.validator.address}><Avatar moniker={moniker} identity={props.validator.description.identity} address={props.validator.address} list={true} />{moniker}</Link> {props.validator.description.identity? <KeybaseCheck identity={props.validator.description.identity} />:''}</td>
         <td className="voting-power">{numeral(props.validator.voting_power).format('0,0')} ({numeral(props.validator.voting_power/props.totalPower*100).format('0.00')}%)</td>
+        <td className="status">{props.validator.jailed?<Badge color="danger"><span className="d-none d-sm-inline">Jailed</span></Badge>:<Badge color="success"><span className="d-none d-sm-inline">Active</span></Badge>}</td>
         <td className="uptime"><Progress animated value={props.validator.uptime}>{props.validator.uptime?props.validator.uptime.toFixed(2):0}%</Progress></td>
         <td>{(props.validator.lastSeen)?moment.utc(props.validator.lastSeen).format("D MMM YYYY, h:mm:ssa z"):''}</td>
     </tr>
@@ -31,7 +32,13 @@ export default class List extends Component{
             if (this.props.validators.length > 0){
                 this.setState({
                     validators: this.props.validators.map((validator, i) => {
-                        return <ValidatorRow key={i} index={i} validator={validator} address={validator.address} totalPower={this.props.chainStatus.totalVotingPower}/>
+                        return <ValidatorRow 
+                            key={i} 
+                            index={i} 
+                            validator={validator} 
+                            address={validator.address} 
+                            totalPower={this.props.chainStatus.totalVotingPower}    
+                        />
                     })
                 })    
             }
--- imports/ui/validators/ValidatorsList.jsx
+++ imports/ui/validators/ValidatorsList.jsx
@@ -20,6 +20,7 @@ export default class Validators extends Component{
                                 <th className="d-none d-md-table-cell counter">&nbsp;</th>
                                 <th className="moniker"><i className="material-icons">perm_contact_calendar</i> <span className="d-none d-sm-inline">Moniker</span></th>
                                 <th className="voting-power"><i className="material-icons">power</i> <span className="d-none d-sm-inline">Voting Power</span></th>
+                                <th className="status"><i class="material-icons">toggle_on</i> <span className="d-none d-sm-inline">Status</span></th>
                                 <th className="uptime"><i className="material-icons">flash_on</i> <span className="d-none d-sm-inline">Uptime (last {Meteor.settings.public.uptimeWindow} blocks)</span></th>
                                 <th className="last-seen"><i className="material-icons">access_time</i> <span className="d-none d-sm-inline">Last Seen</span></th>
                             </tr>
