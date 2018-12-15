9b01039e9309e1d5fcd90bbf0f7fee18a345abe8
Kwun Yeung <kwun@forbole.com>
2018-12-15 21:28:20 +0800

    update number format

--- imports/ui/blocks/block.js
+++ imports/ui/blocks/block.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-// import { Table } from 'reactstrap';
 import moment from 'moment';
+import numeral from 'numeral';
 import Avatar from '../components/Avatar.jsx';
 import KeybaseCheck from '../components/KeybaseCheck.jsx';
 
@@ -13,10 +13,10 @@ export default class Block extends Component {
     render() {
         return (
             <tr className="blockrow">
-                <td className="innerblock1">{this.props.block.height}</td>
+                <td className="innerblock1">{numeral(this.props.block.height).format('0,0')}</td>
                 <td className="innerblock2">{this.props.block.hash}</td>
                 <td className=""><Link to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={this.props.block.proposer().description.moniker} identity={this.props.block.proposer().description.identity} address={this.props.block.proposerAddress} list={true} /> {this.props.block.proposer().description.moniker}</Link> {this.props.block.proposer().description.identity? <KeybaseCheck identity={this.props.block.proposer().description.identity} />:''}</td>
-                <td xs="3" className="innerblock">{this.props.block.transNum}</td>
+                <td xs="3" className="innerblock">{numeral(this.props.block.transNum).format('0,0')}</td>
                 <td xs="3" className="innerblock">{moment.utc(this.props.block.time).format("D MMM YYYY, h:mm:ssa")}</td>
             </tr>
         );
--- imports/ui/components/PowerHistory.jsx
+++ imports/ui/components/PowerHistory.jsx
@@ -28,7 +28,7 @@ export default class PowerHistory extends React.Component {
             <Row>
                 <Col xs={2} className={(this.props.type == 'down')?'text-danger':(this.props.type == 'up'?'text-success':'text-warning')}><i className={changeClass}></i></Col>
                 <Col xs={10} sm={6} ><span className="voting-power">{numeral(this.props.prevVotingPower).format('0,0')}</span> <i className="material-icons text-info">arrow_forward</i> <span className="voting-power">{numeral(this.props.votingPower).format('0,0')}</span></Col>
-                <Col xs={{size:10, offset:2}} sm={{offset:0, size:4}} className="text-secondary"><i className="fas fa-cube"></i> {this.props.height}<br/><i className="far fa-clock"></i> {momemt(this.props.time).format("D MMM YYYY, h:mm:ssa z")}</Col>
+                <Col xs={{size:10, offset:2}} sm={{offset:0, size:4}} className="text-secondary"><i className="fas fa-cube"></i> {numeral(this.props.height).format('0,0')}<br/><i className="far fa-clock"></i> {momemt(this.props.time).format("D MMM YYYY, h:mm:ssa z")}</Col>
             </Row>
         </Card>
     );
