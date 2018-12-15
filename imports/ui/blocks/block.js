1f5862e1304d97debc0777a8274fe2bf62bcb958
Kwun Yeung <kwun@forbole.com>
2018-12-15 20:35:49 +0800

    fix typo

--- imports/ui/blocks/block.js
+++ imports/ui/blocks/block.js
@@ -13,7 +13,7 @@ export default class Block extends Component {
             <tr className="blockrow">
                 <td className="innerblock1">{this.props.block.height}</td>
                 <td className="innerblock2">{this.props.block.hash}</td>
-                <td className=""><Link to={"/validator/"+this.props.block.proposorAddress}>{this.props.block.proposer().description.moniker}</Link></td>
+                <td className=""><Link to={"/validator/"+this.props.block.proposerAddress}>{this.props.block.proposer().description.moniker}</Link></td>
                 <td xs="3" className="innerblock">{this.props.block.transNum}</td>
                 <td xs="3" className="innerblock">{moment.utc(this.props.block.time).format("D MMM YYYY, h:mm:ssa")}</td>
             </tr>
