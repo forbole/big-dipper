1440a55442deaa997fec2c7b68ad15dfc8988518
Kwun Yeung <kwun@forbole.com>
2018-12-15 20:46:28 +0800

    add avatar to block list

--- imports/ui/blocks/block.js
+++ imports/ui/blocks/block.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import { Table } from 'reactstrap';
 import moment from 'moment';
+import Avatar from '../components/Avatar.jsx';
+import KeybaseCheck from '../components/KeybaseCheck.jsx';
 
 export default class Block extends Component {
     constructor(props){
@@ -13,7 +15,7 @@ export default class Block extends Component {
             <tr className="blockrow">
                 <td className="innerblock1">{this.props.block.height}</td>
                 <td className="innerblock2">{this.props.block.hash}</td>
-                <td className=""><Link to={"/validator/"+this.props.block.proposerAddress}>{this.props.block.proposer().description.moniker}</Link></td>
+                <td className=""><Link to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={this.props.block.proposer().description.moniker} identity={this.props.block.proposer().description.identity} address={this.props.block.proposerAddress} list={true} /> {this.props.block.proposer().description.moniker}</Link> {this.props.block.proposer().description.identity? <KeybaseCheck identity={this.props.block.proposer().description.identity} />:''}</td>
                 <td xs="3" className="innerblock">{this.props.block.transNum}</td>
                 <td xs="3" className="innerblock">{moment.utc(this.props.block.time).format("D MMM YYYY, h:mm:ssa")}</td>
             </tr>
