ac3d5950e41272b087981f4e4a1c4f6d8a9ba416
Kwun Yeung <kwun@forbole.com>
2019-01-21 02:28:40 +0800

    remove txs console message

--- imports/api/transactions/server/methods.js
+++ imports/api/transactions/server/methods.js
@@ -9,9 +9,7 @@ Meteor.methods({
         let url = LCD+ '/txs/'+hash;
         let response = HTTP.get(url);
         let tx = JSON.parse(response.content);
-        console.log(hash);
 
-        // console.log(tx.tx.value.msg[0].type);
         if (tx.result.tags.length > 0){
             tx.result.tags.map((tag, i) => {
                 let key = Buffer.from(tag.key, 'base64').toString();
@@ -27,8 +25,6 @@ Meteor.methods({
                 }
             });    
         }
-
-        // console.log(tx.result.tags);
 
         let txId = Transactions.insert(tx);
         if (txId){
