7a7cc96599735d9f383fd3d4848efbc2bd7c319b
Kwun Yeung <kwun@forbole.com>
2019-02-08 14:29:15 +0800

    update validator list

--- imports/ui/validators/List.jsx
+++ imports/ui/validators/List.jsx
@@ -11,13 +11,13 @@ const ValidatorRow = (props) => {
     let identity = (props.validator.description&&props.validator.description.identity)?props.validator.description.identity:'';
     return <Card body>
         <Row className="validator-info">
-            <Col className="d-md-block counter data" xs={2} md={1}>{props.index+1}.</Col>
-            <Col xs={10} md={3} className="data"><Link to={"/validator/"+props.validator.address}><Avatar moniker={moniker} identity={identity} address={props.validator.address} list={true} />{moniker}</Link></Col>
-            <Col className="voting-power data" xs={6} md={2}>{numeral(props.validator.voting_power).format('0,0')} ({numeral(props.validator.voting_power/props.totalPower*100).format('0.00')}%)</Col>
-            <Col className="status data" xs={2} md={1}>{props.validator.jailed?<Badge color="danger"><span>Jailed</span></Badge>:<Badge color="success"><span>Active</span></Badge>}</Col>
+            <Col className="d-none d-md-block counter data" xs={2} md={1}>{props.index+1}</Col>
+            <Col xs={12} md={3} className="data"><Link to={"/validator/"+props.validator.address}><Avatar moniker={moniker} identity={identity} address={props.validator.address} list={true} /><span className="moniker">{moniker}</span></Link></Col>
+            <Col className="voting-power data" xs={{size:4, offset:2}} md={{size:2, offset:0}}>{numeral(props.validator.voting_power).format('0,0')}<span className="d-none d-md-inline"> ({numeral(props.validator.voting_power/props.totalPower*100).format('0.00')}%)</span></Col>
+            <Col className="status data" xs={2} md={1}>{props.validator.jailed?<Badge color="danger"><span>J<span className="d-none d-md-inline">ailed</span></span></Badge>:<Badge color="success"><span>A<span className="d-none d-md-inline">ctive</span></span></Badge>}</Col>
             {(!props.jailed)?<Col className="uptime data" xs={4} md={3}><Progress animated value={props.validator.uptime}>{props.validator.uptime?props.validator.uptime.toFixed(2):0}%</Progress></Col>:''}
             {(!props.jailed)?<Col className="proposer-priority data d-none d-md-block">{numeral(props.validator.proposer_priority).format('0,0')}</Col>:''}
-            {(props.jailed)?<Col className="last-seen data" md={5}>{props.validator.lastSeen?moment.utc(props.validator.lastSeen).format("D MMM YYYY, h:mm:ssa"):''}</Col>:''}
+            {(props.jailed)?<Col className="last-seen data" xs={{size:10,offset:2}}md={{size:5, offset:0}}>{props.validator.lastSeen?moment.utc(props.validator.lastSeen).format("D MMM YYYY, h:mm:ssa"):''}</Col>:''}
         </Row>
     </Card>
 }
