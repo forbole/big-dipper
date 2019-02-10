8ebdc5a106c991a2d4261338f4baa17febed4fdc
Kwun Yeung <kwun@forbole.com>
2019-02-11 05:00:47 +0800

    display txs as activities

---
+++ imports/ui/components/Activities.jsx
@@ -0,0 +1,74 @@
+import React, {Component } from 'react';
+import { MsgType } from './MsgType.jsx';
+import { Link } from 'reactstrap';
+import numeral from 'numeral';
+
+export default class Activites extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        console.log(this.props);
+        let msg = this.props.msg;
+        switch (msg.type){
+            // bank
+            case "cosmos-sdk/Send":
+                let amount = '';
+                for (let a in msg.value.amount){
+                    if (a > 0){
+                        amount += ', '+numeral(msg.value.amount[a].amount).format("0,0")+" "+msg.value.amount[a].denom;
+                    }
+                    else{
+                        amount += numeral(msg.value.amount[a].amount).format("0,0")+" "+msg.value.amount[a].denom;
+                    }
+                }
+                return <p>{msg.value.from_address} <MsgType type={msg.type} /> <em className="text-success">{amount}</em> to {msg.value.to_address}.</p>
+            case "cosmos-sdk/MultiSend":
+                return <MsgType type={msg.type} />
+            
+            // staking
+            case "cosmos-sdk/MsgCreateValidator":
+                return <p>{msg.value.delegator_address} <MsgType type={msg.type} /> operating at {msg.value.validator_address} with moniker <Link to="#">{msg.value.description.moniker}</Link>.</p>
+            case "cosmos-sdk/MsgEditValidator":
+                return <MsgType type={msg.type} />
+            case "cosmos-sdk/MsgDelegate":
+                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.value.amount).format("0,0")} {msg.value.value.denom}</em> to {msg.value.validator_addr}.</p>
+            case "cosmos-sdk/Undelegate":
+                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.shares_amount).format("0,0")} </em> from {msg.value.validator_addr}.</p>
+            case "cosmos-sdk/BeginRedelegate":
+                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.shares_amount).format("0.0")}</em> from {msg.value.validator_src_addr} to {msg.value.validator_dst_addr}.</p>
+            
+            // gov
+            case "cosmos-sdk/MsgSubmitProposal":
+                return <MsgType type={msg.type} />
+            case "cosmos-sdk/MsgDeposit":
+                return <MsgType type={msg.type} />
+            case "cosmos-sdk/MsgVote":
+                return <MsgType type={msg.type} />
+            
+            // distribution
+            case "cosmos-sdk/MsgWithdrawValidatorCommission":
+                return <p>{msg.value.validator_addr} <MsgType type={msg.type} />.</p>
+            case "cosmos-sdk/MsgWithdrawDelegationReward":
+                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> from {msg.value.validator_addr}.</p>
+            case "cosmos-sdk/MsgModifyWithdrawAddress":
+                return <MsgType type={msg.type} />
+    
+            // slashing
+            case "cosmos-sdk/MsgUnjail":
+                return <p>{msg.value.address} <MsgType type={msg.type} />.</p>
+            
+            // ibc
+            case "cosmos-sdk/IBCTransferMsg":
+                return <MsgType type={msg.type} />
+            case "cosmos-sdk/IBCReceiveMsg":
+                return <MsgType type={msg.type} />
+    
+            default:
+                return <ul>
+                    <li>{JSON.stringify(msg.value)}</li>
+                </ul>
+        }
+    }
+}
--- imports/ui/transactions/List.jsx
+++ imports/ui/transactions/List.jsx
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, UncontrolledTooltip } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import { TxIcon } from '../components/Icons.jsx';
-import { MsgType } from '../components/MsgType.jsx';
+import Activities from '../components/Activities.jsx';
 import TimeAgo from '../components/TimeAgo.jsx';
 import numeral from 'numeral';
 
 const TransactionRow = (props) => {
-    console.log(props.tx);
     let tx = props.tx;
+    // console.log(tx);
     return <Row className="tx-info">
-        <Col xs={9} lg={7}>{(tx.tx.value.msg && tx.tx.value.msg.length >0)?tx.tx.value.msg.map((msg,i) => {
-            return <div><MsgType key={i} type={msg.type}/></div>
+        <Col xs={9} lg={7} className="activity">{(tx.tx.value.msg && tx.tx.value.msg.length >0)?tx.tx.value.msg.map((msg,i) => {
+            return <div key={i}><Activities msg={msg}/></div>
         }):''}</Col>
         <Col xs={3} lg={{size:1,order:"last"}} className="text-truncate"><Link to="#">{tx.txhash}</Link></Col>
-        <Col xs={6} md={9} lg={{size:2,order:"last"}}>{tx.block()?<TimeAgo time={tx.block().time} />:''}</Col>
-        <Col xs={4} md={2} lg={1}><Link to="#">{numeral(tx.height).format(0,0)}</Link></Col>
+        <Col xs={6} md={9} lg={{size:2,order:"last"}}><i className="material-icons">schedule</i> <span>{tx.block()?<TimeAgo time={tx.block().time} />:''}</span></Col>
+        <Col xs={4} md={2} lg={1}><i className="fas fa-database d-lg-none"></i> <Link to="#">{numeral(tx.height).format(0,0)}</Link></Col>
         <Col xs={2} md={1}>{(!tx.code)?<TxIcon valid />:<TxIcon />}</Col>
-        <Col xs={12} lg={2}>{tx.tx.value.fee.amount?tx.tx.value.fee.amount.map((fee,i) => {
+        <Col xs={12} lg={2}><i className="material-icons d-lg-none">monetization_on</i> <span>{tx.tx.value.fee.amount?tx.tx.value.fee.amount.map((fee,i) => {
             return <p>{numeral(fee.amount).format(0,0)} {fee.denom}</p>
-        }):'No fee'}</Col>
+        }):'No fee'}</span></Col>
     </Row>
 }
 
@@ -56,10 +56,9 @@ export default class Transactions extends Component{
         }
         else{
             return <div className="transactions-list">
-                <Row className="header text-nowrap d-none d-md-flex">
+                <Row className="header text-nowrap d-none d-lg-flex">
                     <Col xs={9} lg={7}><i className="material-icons">message</i> <span className="d-none d-md-inline-block">Activities</span></Col>
                     <Col xs={3} lg={{size:1,order:"last"}}><i className="fas fa-hashtag"></i> <span className="d-none d-md-inline-block">Tx Hash</span></Col>
-                    <Col xs={6} md={9} lg={{size:2,order:"last"}}><i className="material-icons">schedule</i> <span className="d-none d-md-inline-block">Age</span></Col>
                     <Col xs={4} md={2} lg={1}><i className="fas fa-database"></i> <span className="d-none d-md-inline-block">Height</span></Col>
                     <Col xs={2} md={1} className="text-nowrap"><i className="material-icons">check_circle</i> <span className="d-none d-lg-inline-block">Valid</span></Col>
                     <Col xs={12} lg={2}><i className="material-icons">monetization_on</i> <span className="d-none d-md-inline-block">Fee</span></Col>
