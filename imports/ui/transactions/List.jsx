5e3a4cf0a4356e9e982c7c1225be3a21587651ed
Kwun Yeung <kwun@forbole.com>
2019-02-14 16:36:52 +0800

    update tx list display

--- imports/ui/transactions/List.jsx
+++ imports/ui/transactions/List.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col } from 'reactstrap';
+import { Row, Col, Card, CardBody } from 'reactstrap';
 import { TxIcon } from '../components/Icons.jsx';
 import Activities from '../components/Activities.jsx';
 import TimeAgo from '../components/TimeAgo.jsx';
@@ -9,17 +9,17 @@ import numeral from 'numeral';
 const TransactionRow = (props) => {
     let tx = props.tx;
     // console.log(tx);
-    return <Row className="tx-info">
-        <Col xs={9} lg={7} className="activity">{(tx.tx.value.msg && tx.tx.value.msg.length >0)?tx.tx.value.msg.map((msg,i) => {
-            return <div key={i}><Activities msg={msg}/></div>
+    return <Row className={(tx.code)?"tx-info invalid":"tx-info"}>
+        <Col xs={12} lg={7} className="activity">{(tx.tx.value.msg && tx.tx.value.msg.length >0)?tx.tx.value.msg.map((msg,i) => {
+            return <Card body key={i}><Activities msg={msg}/></Card>
         }):''}</Col>
-        <Col xs={3} lg={{size:1,order:"last"}} className="text-truncate"><Link to="#">{tx.txhash}</Link></Col>
+        <Col xs={{size:6,order:"last"}} md={{size:3, order: "last"}} lg={{size:1,order:"last"}} className="text-truncate"><i className="fas fa-hashtag d-lg-none"></i> <Link to="#">{tx.txhash}</Link></Col>
         <Col xs={6} md={9} lg={{size:2,order:"last"}}><i className="material-icons">schedule</i> <span>{tx.block()?<TimeAgo time={tx.block().time} />:''}</span></Col>
         <Col xs={4} md={2} lg={1}><i className="fas fa-database d-lg-none"></i> <Link to="#">{numeral(tx.height).format(0,0)}</Link></Col>
         <Col xs={2} md={1}>{(!tx.code)?<TxIcon valid />:<TxIcon />}</Col>
-        <Col xs={12} lg={2}><i className="material-icons d-lg-none">monetization_on</i> <span>{tx.tx.value.fee.amount?tx.tx.value.fee.amount.map((fee,i) => {
-            return <p>{numeral(fee.amount).format(0,0)} {fee.denom}</p>
-        }):'No fee'}</span></Col>
+        <Col xs={6} md={9} lg={2}><i className="material-icons d-lg-none">monetization_on</i> {tx.tx.value.fee.amount?tx.tx.value.fee.amount.map((fee,i) => {
+            return <span className="text-nowrap">{numeral(fee.amount).format(0,0)} {fee.denom}</span>
+        }):<span>No fee</span>}</Col>
     </Row>
 }
 
