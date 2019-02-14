ca5046547a85be39b983ca0304669d2ab790b660
Kwun Yeung <kwun@forbole.com>
2019-02-15 02:19:24 +0800

    more readible format on txs

--- imports/ui/components/Activities.jsx
+++ imports/ui/components/Activities.jsx
@@ -9,7 +9,7 @@ export default class Activites extends Component {
     }
 
     render(){
-        console.log(this.props);
+        // console.log(this.props);
         let msg = this.props.msg;
         switch (msg.type){
             // bank
@@ -23,21 +23,21 @@ export default class Activites extends Component {
                         amount += numeral(msg.value.amount[a].amount).format("0,0")+" "+msg.value.amount[a].denom;
                     }
                 }
-                return <p>{msg.value.from_address} <MsgType type={msg.type} /> <em className="text-success">{amount}</em> to {msg.value.to_address}.</p>
+                return <p>{msg.value.from_address} {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} /> <em className="text-success">{amount}</em> to <span className="address">{msg.value.to_address}</span>.</p>
             case "cosmos-sdk/MultiSend":
                 return <MsgType type={msg.type} />
             
             // staking
             case "cosmos-sdk/MsgCreateValidator":
-                return <p>{msg.value.delegator_address} <MsgType type={msg.type} /> operating at {msg.value.validator_address} with moniker <Link to="#">{msg.value.description.moniker}</Link>.</p>
+                return <p>{msg.value.delegator_address} {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} /> operating at <span className="address">{msg.value.validator_address}</span> with moniker <Link to="#">{msg.value.description.moniker}</Link>.</p>
             case "cosmos-sdk/MsgEditValidator":
                 return <MsgType type={msg.type} />
             case "cosmos-sdk/MsgDelegate":
-                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.value.amount).format("0,0")} {msg.value.value.denom}</em> to {msg.value.validator_addr}.</p>
+                return <p><span className="address">{msg.value.delegator_addr}</span> {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.value.amount).format("0,0")} {msg.value.value.denom}</em> to <span className="address">{msg.value.validator_addr}</span>.</p>
             case "cosmos-sdk/Undelegate":
-                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.shares_amount).format("0,0")} </em> from {msg.value.validator_addr}.</p>
+                return <p><span className="address">{msg.value.delegator_addr}</span> {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.shares_amount).format("0,0")} </em> from <span className="address">{msg.value.validator_addr}</span>.</p>
             case "cosmos-sdk/BeginRedelegate":
-                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.shares_amount).format("0.0")}</em> from {msg.value.validator_src_addr} to {msg.value.validator_dst_addr}.</p>
+                return <p><span className="address">{msg.value.delegator_addr}</span> {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} /> <em className="text-warning">{numeral(msg.value.shares_amount).format("0.0")}</em> from <span className="address">{msg.value.validator_src_addr}</span> to <span className="address">{msg.value.validator_dst_addr}</span>.</p>
             
             // gov
             case "cosmos-sdk/MsgSubmitProposal":
@@ -49,15 +49,15 @@ export default class Activites extends Component {
             
             // distribution
             case "cosmos-sdk/MsgWithdrawValidatorCommission":
-                return <p>{msg.value.validator_addr} <MsgType type={msg.type} />.</p>
+                return <p><span className="address">{msg.value.validator_addr}</span> {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} />.</p>
             case "cosmos-sdk/MsgWithdrawDelegationReward":
-                return <p>{msg.value.delegator_addr} <MsgType type={msg.type} /> from {msg.value.validator_addr}.</p>
+                return <p><span className="address">{msg.value.delegator_addr}</span> {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} /> from <span className="address">{msg.value.validator_addr}</span>.</p>
             case "cosmos-sdk/MsgModifyWithdrawAddress":
                 return <MsgType type={msg.type} />
     
             // slashing
             case "cosmos-sdk/MsgUnjail":
-                return <p>{msg.value.address} <MsgType type={msg.type} />.</p>
+                return <p><span className="address">{msg.value.address}</span> {(this.props.invalid)?"failed to ":''}<MsgType type={msg.type} />.</p>
             
             // ibc
             case "cosmos-sdk/IBCTransferMsg":
--- imports/ui/transactions/List.jsx
+++ imports/ui/transactions/List.jsx
@@ -11,7 +11,7 @@ const TransactionRow = (props) => {
     // console.log(tx);
     return <Row className={(tx.code)?"tx-info invalid":"tx-info"}>
         <Col xs={12} lg={7} className="activity">{(tx.tx.value.msg && tx.tx.value.msg.length >0)?tx.tx.value.msg.map((msg,i) => {
-            return <Card body key={i}><Activities msg={msg}/></Card>
+            return <Card body key={i}><Activities msg={msg} invalid={(!!tx.code)}/></Card>
         }):''}</Col>
         <Col xs={{size:6,order:"last"}} md={{size:3, order: "last"}} lg={{size:1,order:"last"}} className="text-truncate"><i className="fas fa-hashtag d-lg-none"></i> <Link to="#">{tx.txhash}</Link></Col>
         <Col xs={6} md={9} lg={{size:2,order:"last"}}><i className="material-icons">schedule</i> <span>{tx.block()?<TimeAgo time={tx.block().time} />:''}</span></Col>
