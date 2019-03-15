3288bf598784dd3bdeb6778b936ef8e935553ff6
Kwun Yeung <kwun@forbole.com>
2019-03-15 20:23:14 +0800

    fix proposer not ready in blocks

--- imports/ui/blocks/block.js
+++ imports/ui/blocks/block.js
@@ -11,16 +11,21 @@ export default class Block extends Component {
 
     render() {
         let proposer = this.props.block.proposer();
-        let moniker = (proposer&&proposer.description&&proposer.description.moniker)?proposer.description.moniker:proposer.address;
-        let identity = (proposer&&proposer.description&&proposer.description.identity)?proposer.description.identity:"";
-        return (
-            <tr className="blockrow">
-                <td className="innerblock1">{numeral(this.props.block.height).format('0,0')}</td>
-                <td className="innerblock2">{this.props.block.hash}</td>
-                <td className=""><Link to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={moniker} identity={identity} address={this.props.block.proposerAddress} list={true} /> {moniker}</Link></td>
-                <td xs="3" className="innerblock">{numeral(this.props.block.transNum).format('0,0')}</td>
-                <td xs="3" className="innerblock">{moment.utc(this.props.block.time).format("D MMM YYYY, h:mm:ssa")}</td>
-            </tr>
-        );
+        if (proposer){
+            let moniker = (proposer.description&&proposer.description.moniker)?proposer.description.moniker:proposer.address;
+            let identity = (proposer.description&&proposer.description.identity)?proposer.description.identity:"";
+            return (
+                <tr className="blockrow">
+                    <td className="innerblock1">{numeral(this.props.block.height).format('0,0')}</td>
+                    <td className="innerblock2">{this.props.block.hash}</td>
+                    <td className=""><Link to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={moniker} identity={identity} address={this.props.block.proposerAddress} list={true} /> {moniker}</Link></td>
+                    <td xs="3" className="innerblock">{numeral(this.props.block.transNum).format('0,0')}</td>
+                    <td xs="3" className="innerblock">{moment.utc(this.props.block.time).format("D MMM YYYY, h:mm:ssa")}</td>
+                </tr>
+            );                
+        }
+        else{
+            return <tr className="blockrow"></tr>
+        }
     }
 }
