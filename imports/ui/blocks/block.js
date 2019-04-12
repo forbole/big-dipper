59c9167457afdb1a7eb6fc0e8af20d2180b0a7c4
Kwun Yeung <kwun@forbole.com>
2019-04-12 17:07:27 +0800

    restyle blocks

--- imports/ui/blocks/block.js
+++ imports/ui/blocks/block.js
@@ -16,11 +16,11 @@ export default class Block extends Component {
             let moniker = (proposer.description&&proposer.description.moniker)?proposer.description.moniker:proposer.address;
             let identity = (proposer.description&&proposer.description.identity)?proposer.description.identity:"";
             return <Row className="block-info">
-                    <Col sm={2}><i className="fas fa-database d-sm-none"></i> {numeral(this.props.block.height).format('0,0')}</Col>
-                    <Col sm={2} className="text-truncate"><i className="fas fa-hashtag d-sm-none"></i>{ this.props.block.hash}</Col>
-                    <Col sm={3} md={2} lg={3} className="text-truncate"><Link to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={moniker} identity={identity} address={this.props.block.proposerAddress} list={true} /> {moniker}</Link></Col>
-                    <Col sm={1} md={2}><i className="fas fa-sync d-sm-none"></i> {numeral(this.props.block.transNum).format('0,0')}</Col>
-                    <Col sm={4} lg={3}><i className="far fa-clock d-sm-none"></i> {moment.utc(this.props.block.time).format("D MMM YYYY, h:mm:ssa")}</Col>
+                    <Col xs={8} sm={4} lg={3}><i className="far fa-clock d-sm-none"></i> {moment.utc(this.props.block.time).format("D MMM YYYY, h:mm:ssa")}</Col>
+                    <Col xs={4} sm={2} className="text-truncate"><i className="fas fa-hashtag d-sm-none"></i> { this.props.block.hash}</Col>
+                    <Col xs={8}sm={3} md={2} lg={3} className="text-truncate"><Link to={"/validator/"+this.props.block.proposerAddress}><Avatar moniker={moniker} identity={identity} address={this.props.block.proposerAddress} list={true} /> {moniker}</Link></Col>
+                    <Col xs={4} sm={1} md={2}><i className="fas fa-sync d-sm-none"></i> {numeral(this.props.block.transNum).format('0,0')}</Col>
+                    <Col xs={{size:4, offset:8}} sm={{size:2, offset:0}}><i className="fas fa-database d-sm-none"></i> <Link to={"/blocks/"+this.props.block.height}>{numeral(this.props.block.height).format('0,0')}</Link></Col>
                 </Row>    
         }
         else{
