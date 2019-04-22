c247cca691aa95d8720fb20f3bb32b38c740a0c9
Kwun Yeung <kwun@forbole.com>
2019-04-22 21:14:59 +0800

    add average data collection

--- imports/api/records/records.js
+++ imports/api/records/records.js
@@ -5,6 +5,7 @@ export const ValidatorRecords = new Mongo.Collection('validator_records');
 export const Analytics = new Mongo.Collection('analytics');
 export const MissedBlocksStats = new Mongo.Collection('missed_blocks_stats');
 export const VPDistributions = new Mongo.Collection('voting_power_distributions');
+export const AverageData = new Mongo.Collection('average_data');
 
 MissedBlocksStats.helpers({
     proposerMoniker(){
