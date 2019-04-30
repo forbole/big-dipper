6b3aa281624d211c2406c166d45624aca7b6c617
Kwun Yeung <kwun@forbole.com>
2019-04-30 21:50:25 +0800

    restyle account details distribution

--- imports/ui/accounts/Account.jsx
+++ imports/ui/accounts/Account.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Spinner, Row, Col, Card, CardHeader, CardBody } from 'reactstrap';
+import { Spinner, Row, Col, Card, CardHeader, CardBody, Progress } from 'reactstrap';
 import numbro from 'numbro';
 import AccountCopy from '../components/AccountCopy.jsx';
 
@@ -7,7 +7,7 @@ export default class AccountDetails extends Component{
     constructor(props){
         super(props);
         this.state = {
-            loading: false,
+            loading: true,
             accountExists: false,
             available: 0,
             delegated: 0,
@@ -91,25 +91,42 @@ export default class AccountDetails extends Component{
                     <Col><Card>
                         <CardHeader>Balance</CardHeader>
                         <CardBody>
-                            <Row>
-                                <Col md={4} className="label">Available</Col>
-                                <Col md={8} className="value">{numbro(this.state.available/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
+                            <Row className="account-distributions">
+                                <Col xs={12}>
+                                    <Progress multi>
+                                        <Progress bar className="available" value={this.state.available/this.state.total*100} />
+                                        <Progress bar className="delegated" value={this.state.delegated/this.state.total*100} />
+                                        <Progress bar className="unbonding" value={this.state.unbonding/this.state.total*100} />
+                                        <Progress bar className="rewards" value={this.state.rewards/this.state.total*100} />
+                                    </Progress>
+                                </Col>
                             </Row>
                             <Row>
-                                <Col md={4} className="label">Delegated</Col>
-                                <Col md={8} className="value">{numbro(this.state.delegated/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
-                            </Row>
-                            <Row>
-                                <Col md={4} className="label">Unbonding</Col>
-                                <Col md={8} className="value">{numbro(this.state.unbonding/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
-                            </Row>
-                            <Row>
-                                <Col md={4} className="label">Rewards</Col>
-                                <Col md={8} className="value">{numbro(this.state.rewards/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
-                            </Row>
-                            <Row>
-                                <Col md={4} className="label">Total</Col>
-                                <Col md={8} className="value">{numbro(this.state.total/Meteor.settings.public.stakingFraction).format("0,0.000")} {Meteor.settings.public.stakingDenom}s</Col>
+                                <Col md={6} lg={8}>
+                                    <Row>
+                                        <Col xs={4} className="label text-nowrap"><div className="available infinity" />Available</Col>
+                                        <Col xs={8} className="value text-right">{numbro(this.state.available/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={4} className="label text-nowrap"><div className="delegated infinity" />Delegated</Col>
+                                        <Col xs={8} className="value text-right">{numbro(this.state.delegated/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={4} className="label text-nowrap"><div className="unbonding infinity" />Unbonding</Col>
+                                        <Col xs={8} className="value text-right">{numbro(this.state.unbonding/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={4} className="label text-nowrap"><div className="rewards infinity" />Rewards</Col>
+                                        <Col xs={8} className="value text-right">{numbro(this.state.rewards/Meteor.settings.public.stakingFraction).format("0,0.000")}</Col>
+                                    </Row>
+                                </Col>
+                                <Col md={6} lg={4} className="total d-flex flex-column justify-content-end">
+                                    <Row>
+                                        <Col xs={4} className="label d-flex align-self-end"><div className="infinity" />Total</Col>
+                                        <Col xs={8} className="value text-right">{numbro(this.state.total/Meteor.settings.public.stakingFraction).format("0,0.000a")} {Meteor.settings.public.stakingDenom}s</Col>
+                                        <Col xs={12} className="dollar-value text-right text-secondary">(~{numbro(this.state.total/Meteor.settings.public.stakingFraction*3.9).format("$0,0.000a")})</Col>
+                                    </Row>
+                                </Col>
                             </Row>
                         </CardBody>
                     </Card></Col>
