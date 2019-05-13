39d17810a15c04291332c7c6dc872a2ccfc947b4
Kwun Yeung <kwun@forbole.com>
2019-05-13 20:09:31 +0800

    append title and meta to head

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -10,6 +10,7 @@ import { renderToString } from "react-dom/server";
 import { onPageLoad } from 'meteor/server-render';
 import { StaticRouter } from 'react-router-dom';
 import { ServerStyleSheet } from "styled-components"
+import { Helmet } from 'react-helmet';
 
 import App from '../../ui/App.jsx';
 
@@ -24,5 +25,10 @@ onPageLoad(sink => {
       ));
 
     sink.renderIntoElementById('app', html);
+
+    const helmet = Helmet.renderStatic();
+    sink.appendToHead(helmet.meta.toString());
+    sink.appendToHead(helmet.title.toString());
+
     sink.appendToHead(sheet.getStyleTags());
 });
