365e7377e4b3ee8856a51c250cad9f8e0367c9d2
Kwun Yeung <kwun@forbole.com>
2019-05-13 19:08:35 +0800

    block pages

--- imports/ui/blocks/ListContainer.js
+++ imports/ui/blocks/ListContainer.js
@@ -6,10 +6,29 @@ import { Blockscon } from '/imports/api/blocks/blocks.js';
 import Blocks from './List.jsx';
 
 export default BlocksContainer = withTracker((props) => {
-    const heightHandle = Meteor.subscribe('blocks.height', props.limit);
-    const loading = (!heightHandle.ready() && props.limit == Meteor.settings.public.initialPageSize);
-    const blocks = Blockscon.find({}, {sort: {height:-1}}).fetch();
-    const blocksExist = !loading && !!blocks;
+    let heightHandle;
+    let loading = true;
+
+    if (Meteor.isClient){
+        heightHandle = Meteor.subscribe('blocks.height', props.limit);
+        loading = (!heightHandle.ready() && props.limit == Meteor.settings.public.initialPageSize);
+    }
+
+    let blocks;
+    let blocksExist;
+
+    if (Meteor.isServer || !loading){
+        blocks = Blockscon.find({}, {sort: {height:-1}}).fetch();
+        
+        if (Meteor.isServer){
+            loading = false;
+            blocksExist = !!blocks;
+        }
+        else{
+            blocksExist = !loading && !!blocks;
+        }
+    }
+
     return {
         loading,
         blocksExist,
