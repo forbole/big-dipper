e8f08b6277c540ee878922e1a53892236affd526
Kwun Yeung <kwun@forbole.com>
2019-05-13 19:24:54 +0800

    render transaction page

--- imports/ui/transactions/ListContainer.js
+++ imports/ui/transactions/ListContainer.js
@@ -4,11 +4,26 @@ import { Transactions } from '/imports/api/transactions/transactions.js';
 import List from './List.jsx';
 
 export default ValidatorDetailsContainer = withTracker((props) => {
-    const transactionsHandle = Meteor.subscribe('transactions.list', props.limit);
-    const loading = (!transactionsHandle.ready() && props.limit == Meteor.settings.public.initialPageSize);
-    const transactions = Transactions.find({}, {sort:{height:-1}}).fetch();
-    const transactionsExist = !loading && !!transactions;
-    // console.log(props.state.limit);
+    let transactionsHandle, transactions, transactionsExist;
+    let loading = true;
+
+    if (Meteor.isClient){
+        transactionsHandle = Meteor.subscribe('transactions.list', props.limit);
+        loading = (!transactionsHandle.ready() && props.limit == Meteor.settings.public.initialPageSize);
+    }
+
+    if (Meteor.isServer || !loading){
+        transactions = Transactions.find({}, {sort:{height:-1}}).fetch();
+
+        if (Meteor.isServer){
+            loading = false;
+            transactionsExist = !!transactions;
+        }
+        else{
+            transactionsExist = !loading && !!transactions;
+        }
+    }
+    
     return {
         loading,
         transactionsExist,
