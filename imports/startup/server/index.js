6f94bb647dc901aad363e4104934f2b3261c9720
Kwun Yeung <kwun@forbole.com>
2019-05-14 19:02:56 +0800

    remove ssr

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -4,31 +4,31 @@ import './util.js';
 import './register-api.js';
 import './create-indexes.js';
 
-import React from 'react';
+// import React from 'react';
 // import { renderToNodeStream } from 'react-dom/server';
-import { renderToString } from "react-dom/server";
+// import { renderToString } from "react-dom/server";
 import { onPageLoad } from 'meteor/server-render';
-import { StaticRouter } from 'react-router-dom';
-import { ServerStyleSheet } from "styled-components"
+// import { StaticRouter } from 'react-router-dom';
+// import { ServerStyleSheet } from "styled-components"
 import { Helmet } from 'react-helmet';
 
-import App from '../../ui/App.jsx';
+// import App from '../../ui/App.jsx';
 
 onPageLoad(sink => {
-    const context = {};
-    const sheet = new ServerStyleSheet()
+    // const context = {};
+    // const sheet = new ServerStyleSheet()
 
-    const html = renderToString(sheet.collectStyles(
-        <StaticRouter location={sink.request.url} context={context}>
-            <App />
-        </StaticRouter>
-      ));
+    // const html = renderToString(sheet.collectStyles(
+    //     <StaticRouter location={sink.request.url} context={context}>
+    //         <App />
+    //     </StaticRouter>
+    //   ));
 
-    sink.renderIntoElementById('app', html);
+    // sink.renderIntoElementById('app', html);
 
     const helmet = Helmet.renderStatic();
     sink.appendToHead(helmet.meta.toString());
     sink.appendToHead(helmet.title.toString());
 
-    sink.appendToHead(sheet.getStyleTags());
+    // sink.appendToHead(sheet.getStyleTags());
 });
