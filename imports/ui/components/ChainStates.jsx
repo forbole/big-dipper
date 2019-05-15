6a9c0f173c73e9b7c497978d1315aa7b6865ef18
Kwun Yeung <kwun@forbole.com>
2019-05-15 18:06:51 +0800

    hide community pool and inflation

--- imports/ui/components/ChainStates.jsx
+++ imports/ui/components/ChainStates.jsx
@@ -52,7 +52,7 @@ export default class ChainStates extends Component{
         if (this.props.coinStats != prevProps.coinStats){
             if (this.props.coinStats.usd){
                 this.setState({
-                    price: numbro(this.props.coinStats.usd).format("$0,0.00"),
+                    price: numbro(this.props.coinStats.usd).format("$0,0.000000"),
                     marketCap: numbro(this.props.coinStats.usd_market_cap).format("$0,0.00")
                 })
             }
@@ -64,8 +64,8 @@ export default class ChainStates extends Component{
                 <Row className="text-nowrap">
                     <Col xs={4} md="auto"><small><span><T>chainStates.price</T>:</span> <strong>{this.state.price}</strong></small></Col>
                     <Col xs={8} md="auto"><small><span><T>chainStates.marketCap</T>:</span> <strong>{this.state.marketCap}</strong></small></Col>
-                    <Col xs={4} md="auto"><small><span><T>chainStates.inflation</T>:</span> <strong>{this.state.inflation}</strong></small></Col>
-                    <Col xs={8} md="auto"><small><span><T>chainStates.communityPool</T>:</span> <strong>{this.state.communityPool}</strong></small></Col>
+                    {/* <Col xs={4} md="auto"><small><span><T>chainStates.inflation</T>:</span> <strong>{this.state.inflation}</strong></small></Col>
+                    <Col xs={8} md="auto"><small><span><T>chainStates.communityPool</T>:</span> <strong>{this.state.communityPool}</strong></small></Col> */}
                 </Row>
             </CardHeader>
         </Card>
