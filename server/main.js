bd48f70e04ea0d6005b7a50c82feb42082b449c6
Zoe <1817638+ztsai@users.noreply.github.com>
2019-05-20 17:48:48 +0800

    blah

--- server/main.js
+++ server/main.js
@@ -180,7 +180,7 @@ Meteor.startup(function(){
         }
         if (result){
             if (Meteor.settings.debug.startTimer){
-                /*timerConsensus = Meteor.setInterval(function(){
+                timerConsensus = Meteor.setInterval(function(){
                     getConsensusState();
                 }, Meteor.settings.params.consensusInterval);
 
@@ -198,12 +198,12 @@ Meteor.startup(function(){
 
                 timerProposalsResults = Meteor.setInterval(function(){
                     getProposalsResults();
-                }, Meteor.settings.params.proposalInterval);*/
+                }, Meteor.settings.params.proposalInterval);
 
                 timerMissedBlock = Meteor.setInterval(function(){
                     updateMissedBlocks();
                 }, Meteor.settings.params.missedBlocksInterval);
-/*
+
                 timerDelegation = Meteor.setInterval(function(){
                     getDelegations();
                 }, Meteor.settings.params.delegationInterval);
@@ -221,7 +221,7 @@ Meteor.startup(function(){
                     if ((now.getUTCHours() == 5) && (now.getUTCMinutes() == 29) && (now.getUTCSeconds() == 0)){
                         aggregateDaily();
                     }
-                }, 1000)*/
+                }, 1000)
             }
         }
     })
