7022b953c9abecdcb1c4da1acd7e9c8e8c38966d
William Chong <williamchong007@msn.com>
2019-05-30 17:40:08 +0800

    change primary color

--- imports/ui/voting-power/VotingPower.jsx
+++ imports/ui/voting-power/VotingPower.jsx
@@ -42,7 +42,7 @@ export default class VotingPower extends Component{
                 labels.push(this.props.stats[v].description?this.props.stats[v].description.moniker:'');
                 data.push(this.props.stats[v].voting_power);
                 let alpha = (this.props.stats.length+1-v)/this.props.stats.length*0.8+0.2;
-                backgroundColors.push('rgba(189, 8, 28,'+alpha+')');
+                backgroundColors.push('rgba(40, 100, 110,'+alpha+')');
             }
             this.setState({
                 data:{
