046c2f143beaca752ed3af44e7791b17336e2008
Zoe <1817638+ztsai@users.noreply.github.com>
2019-07-15 10:46:23 +0000

    fix chain state height check

--- imports/api/chain/server/methods.js
+++ imports/api/chain/server/methods.js
@@ -52,9 +52,9 @@ Meteor.methods({
             chain.latestBlockHeight = status.sync_info.latest_block_height;
             chain.latestBlockTime = status.sync_info.latest_block_time;
 
-            let lastestState = ChainStates.findOne({}, {sort: {height: -1}})
-            if (lastestState.latest_block_height >= chain.latestBlockHeight) {
-                return `no updates (getting block ${chain.latestBlockHeight} at block ${lastestState.latestBlockHeight})`
+            let latestState = ChainStates.findOne({}, {sort: {height: -1}})
+            if (latestState.height >= chain.latestBlockHeight) {
+                return `no updates (getting block ${chain.latestBlockHeight} at block ${latestState.height})`
             }
 
             url = RPC+'/validators';
