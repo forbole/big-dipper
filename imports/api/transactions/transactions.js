9f5ffcafffe29508e0ae16f292485c9e7a574772
Kwun Yeung <kwun@forbole.com>
2019-07-29 16:11:39 +0800

    remove client side

--- imports/api/transactions/transactions.js
+++ imports/api/transactions/transactions.js
@@ -1,6 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 import { Blockscon } from '../blocks/blocks.js';
-import { TxIcon } from '../../ui/components/Icons.jsx';
 
 export const Transactions = new Mongo.Collection('transactions');
 
