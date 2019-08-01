9a3eb1318c894d30f3c89c12498ee46fc78c1e82
Kwun Yeung <kwun@forbole.com>
2019-08-01 18:31:24 +0800

    check latest state exsits

--- imports/api/chain/server/methods.js
+++ imports/api/chain/server/methods.js
@@ -53,7 +53,7 @@ Meteor.methods({
             chain.latestBlockTime = status.sync_info.latest_block_time;
 
             let latestState = ChainStates.findOne({}, {sort: {height: -1}})
-            if (latestState.height >= chain.latestBlockHeight) {
+            if (latestState && latestState.height >= chain.latestBlockHeight) {
                 return `no updates (getting block ${chain.latestBlockHeight} at block ${latestState.height})`
             }
 
