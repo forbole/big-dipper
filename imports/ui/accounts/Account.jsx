9aae5ab6f837c745a7bc693e34a4261d04526006
Chung Wu <chung@like.co>
2019-09-17 17:15:13 +0800

    Rename strings to LikeChain

--- imports/ui/accounts/Account.jsx
+++ imports/ui/accounts/Account.jsx
@@ -184,8 +184,8 @@ export default class AccountDetails extends Component{
         else if (this.state.accountExists){
             return <div id="account">
                 <Helmet>
-                    <title>Account Details of {this.state.address} on Cosmos Hub | The Big Dipper</title>
-                    <meta name="description" content={"Account Details of "+this.state.address+" on Cosmos Hub"} />
+                    <title>Account Details of {this.state.address} on LikeChain | The Big Dipper</title>
+                    <meta name="description" content={"Account Details of "+this.state.address+" on LikeChain"} />
                 </Helmet>
                 <Row>
                     <Col md={3} xs={12}><h1 className="d-none d-lg-block"><T>accounts.accountDetails</T></h1></Col>
