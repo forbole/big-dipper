154069fb2d33a9752a911464ee78da597cdad92e
jhernandezb <contact@jhernandez.me>
2019-12-03 09:57:38 -0600

    Fixed delegation amount

--- imports/ui/accounts/Account.jsx
+++ imports/ui/accounts/Account.jsx
@@ -66,9 +66,10 @@ export default class AccountDetails extends Component{
                 this.setState({delegations: result.delegations || []})
                 if (result.delegations && result.delegations.length > 0){
                     result.delegations.forEach((delegation, i) => {
+                        const amount = delegation.balance.amount || delegation.balance;
                         this.setState({
-                            delegated: this.state.delegated+parseFloat(delegation.balance),
-                            total: this.state.total+parseFloat(delegation.balance)
+                            delegated: this.state.delegated+parseFloat(amount),
+                            total: this.state.total+parseFloat(amount)
                         })
                     }, this)
                 }
