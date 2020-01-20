6f8ff7fd01b4fe16b721c892d492d5223b7a0063
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-01-20 10:57:57 +0800

    ChainInfo Component Added

---
+++ imports/ui/components/ChainInfo.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Card, CardHeader, Row, Col } from 'reactstrap';
+import i18n from 'meteor/universe:i18n';
+import ChainStates from '../components/ChainStatesContainer.js';
+
+const T = i18n.createComponent();
+
+export default class ChainInfo extends Component{
+    constructor(props){
+        super(props);
+    };
+
+    render(){
+        return(
+        <div className="chainInfo">
+            <Row>
+            <Col md={3} xs={12}><h1>{Meteor.settings.public.chainName}</h1></Col>
+            <Col md={9} xs={12} className="text-md-right"><ChainStates /></Col>
+            </Row>
+        </div>
+        );
+    }
+}
