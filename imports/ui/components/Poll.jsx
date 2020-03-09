2cdcd2e14f8c40898d9320e3506dbe1feebc818c
Kwun Yeung <kwun@forbole.com>
2020-03-09 20:27:12 +0800

    remove alert import in Poll component

--- imports/ui/components/Poll.jsx
+++ imports/ui/components/Poll.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 const Poll = (props) => {
