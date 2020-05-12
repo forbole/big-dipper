e0b8448d03367c9a46976a9fa8a19ccb2cae99ab
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-05-12 19:12:14 +0800

    Updated CDPs List

--- imports/ui/cdp/List.jsx
+++ imports/ui/cdp/List.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import numbro from 'numbro';
 import i18n from 'meteor/universe:i18n';
 import TimeStamp from '../components/TimeStamp.jsx';
+import Coin from '/both/utils/coins.js'
 
 const T = i18n.createComponent();
 
@@ -13,9 +14,13 @@ const CDPRow = (props) => {
     
     return <tr>
         <th className="d-none d-sm-table-cell counter ">{props.cdpList.cdp.id}</th>
-            <td className="title"><Link to={"/account/"+props.cdpList.cdp.owner}>{props.cdpList.cdp.owner}</Link></td>
-            <td className="submit-block"><TimeStamp time={props.cdpList.cdp.fees_updated}/></td>
+            <td className="owner"><Link to={"/account/"+props.cdpList.cdp.owner}>{props.cdpList.cdp.owner}</Link></td>
             <td className="value ">{numbro(parseFloat(props.cdpList.collateralization_ratio)).format('0.000000')}</td>
+            <td className="accumulated-fees">{props.cdpList.cdp.accumulated_fees?props.cdpList.cdp.accumulated_fees.map((deposit, i) => {
+            return <div key={i}>{new Coin(deposit.amount, deposit.denom).toString()}</div>
+        }):'0 usdx'}</td>
+            <td className="fees-updated"><TimeStamp time={props.cdpList.cdp.fees_updated}/></td>
+            
          </tr>
 }
 
@@ -55,9 +60,11 @@ export default class List extends Component{
                         <thead>
                             <tr>
                             <th className="d-none d-sm-table-cell counter "><i className="material-icons">sort</i> <T>cdp.cdpID</T></th>
-                                <th className="d-none d-sm-table-cell title"><i className="material-icons">account_circle</i> <span className="d-none d-sm-inline"><T>cdp.owner</T></span></th>
-                                <th className="submit-block"><i className="material-icons">update</i> <span className="d-none d-sm-inline"><T>cdp.cdpFeesUpdated</T> (UTC)</span></th>
+                                <th className="d-none d-sm-table-cell owner"><i className="material-icons">account_circle</i> <span className="d-none d-sm-inline"><T>cdp.owner</T></span></th>
                                 <th className="value "><i className="material-icons">attach_money</i> <span className="d-none d-sm-inline"><T>cdp.collateralizationRatio</T></span></th>
+                                <th className="accumulated-fees"><i className="material-icons">attach_money</i> <span className="d-none d-sm-inline"><T>cdp.accumulatedFees</T></span></th>
+                                <th className="fees-updated"><i className="material-icons">update</i> <span className="d-none d-sm-inline"><T>cdp.cdpFeesUpdated</T> (UTC)</span></th>
+                               
                             </tr>
                         </thead>
                         <tbody>{this.state.cdpList}</tbody>
