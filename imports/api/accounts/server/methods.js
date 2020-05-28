ea45f9e44130a57480908d3ceb66930a98bc78a1
Kwun Yeung <kwun@forbole.com>
2020-05-28 23:27:50 +0800

    fix cdp display list

--- imports/api/accounts/server/methods.js
+++ imports/api/accounts/server/methods.js
@@ -1,26 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
 import { Validators } from '/imports/api/validators/validators.js';
+
 const fetchFromUrl = (url) => {
-    try{
+    try {
         let res = HTTP.get(LCD + url);
-        if (res.statusCode == 200){
+        if (res.statusCode == 200) {
             return res
         };
-    }
-    catch (e){
+    } catch (e) {
         console.log(res);
         console.log(e);
     }
 }
 
 Meteor.methods({
-    'accounts.getAccountDetail': function(address){
+    'accounts.getAccountDetail': function (address) {
         this.unblock();
-        let url = LCD + '/auth/accounts/'+ address;
-        try{
+        let url = LCD + '/auth/accounts/' + address;
+        try {
             let available = HTTP.get(url);
-            if (available.statusCode == 200){
+            if (available.statusCode == 200) {
                 let response = JSON.parse(available.content).result;
                 let account;
                 if (response.type === 'cosmos-sdk/Account')
@@ -31,89 +31,90 @@ Meteor.methods({
                     return account
                 return null
             }
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e)
         }
     },
-    'accounts.getBalance': function(address){
+    'accounts.getBalance': function (address) {
         this.unblock();
         let balance = {}
 
         // get available atoms
-        let url = LCD + '/bank/balances/'+ address;
-        try{
+        let url = LCD + '/bank/balances/' + address;
+        try {
             let available = HTTP.get(url);
-            if (available.statusCode == 200){
+            if (available.statusCode == 200) {
                 balance.available = JSON.parse(available.content).result;
 
             }
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e)
         }
 
         // get delegated amnounts
-        url = LCD + '/staking/delegators/'+address+'/delegations';
-        try{
+        url = LCD + '/staking/delegators/' + address + '/delegations';
+        try {
             let delegations = HTTP.get(url);
-            if (delegations.statusCode == 200){
+            if (delegations.statusCode == 200) {
                 balance.delegations = JSON.parse(delegations.content).result;
             }
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
         // get unbonding
-        url = LCD + '/staking/delegators/'+address+'/unbonding_delegations';
-        try{
+        url = LCD + '/staking/delegators/' + address + '/unbonding_delegations';
+        try {
             let unbonding = HTTP.get(url);
-            if (unbonding.statusCode == 200){
+            if (unbonding.statusCode == 200) {
                 balance.unbonding = JSON.parse(unbonding.content).result;
             }
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
 
         // get rewards
-        url = LCD + '/distribution/delegators/'+address+'/rewards';
-        try{
+        url = LCD + '/distribution/delegators/' + address + '/rewards';
+        try {
             let rewards = HTTP.get(url);
-            if (rewards.statusCode == 200){
+            if (rewards.statusCode == 200) {
                 //get seperate rewards value
                 balance.rewards = JSON.parse(rewards.content).result.rewards;
                 //get total rewards value
-                balance.total_rewards= JSON.parse(rewards.content).result.total;
-                
+                balance.total_rewards = JSON.parse(rewards.content).result.total;
+
             }
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
 
         // get commission
-        let validator = Validators.findOne(
-            {$or: [{operator_address:address}, {delegator_address:address}, {address:address}]})
+        let validator = Validators.findOne({
+            $or: [{
+                operator_address: address
+            }, {
+                delegator_address: address
+            }, {
+                address: address
+            }]
+        })
         if (validator) {
             let url = LCD + '/distribution/validators/' + validator.operator_address;
             balance.operator_address = validator.operator_address;
             try {
                 let rewards = HTTP.get(url);
-                if (rewards.statusCode == 200){
+                if (rewards.statusCode == 200) {
                     let content = JSON.parse(rewards.content).result;
                     if (content.val_commission && content.val_commission.length > 0)
                         balance.commission = content.val_commission;
 
                 }
 
-            }
-            catch (e){
+            } catch (e) {
                 console.log(url);
                 console.log(e)
             }
@@ -121,56 +122,53 @@ Meteor.methods({
 
         return balance;
     },
-    'accounts.getDelegation'(address, validator){
-    let url = LCD + `/staking/delegators/${address}/delegations/${validator}`;
+    'accounts.getDelegation'(address, validator) {
+        let url = LCD + `/staking/delegators/${address}/delegations/${validator}`;
         let delegations = {};
-        try{
+        try {
             delegations = HTTP.get(url);
-            if (delegations.statusCode == 200){
+            if (delegations.statusCode == 200) {
                 delegations = JSON.parse(delegations.content).result;
                 if (delegations && delegations.shares)
-                delegations.shares = parseFloat(delegations.shares);
+                    delegations.shares = parseFloat(delegations.shares);
             };
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
 
         url = LCD + `/staking/redelegations?delegator=${address}&validator_to=${validator}`;
 
-        try{
+        try {
             let completionTime;
             let relegations = HTTP.get(url);
-            if (relegations.statusCode == 200){
+            if (relegations.statusCode == 200) {
                 relegations = JSON.parse(relegations.content).result;
                 relegations.forEach((relegation) => {
                     let entries = relegation.entries
-                    let time = new Date(entries[entries.length-1].completion_time)
+                    let time = new Date(entries[entries.length - 1].completion_time)
                     if (!completionTime || time > completionTime)
                         completionTime = time
                 })
 
                 delegations.redelegationCompletionTime = completionTime;
             };
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
 
         url = LCD + `/staking/delegators/${address}/unbonding_delegations/${validator}`;
 
-        try{
-           
+        try {
+
             let undelegations = HTTP.get(url);
-            if (undelegations.statusCode == 200){
+            if (undelegations.statusCode == 200) {
                 undelegations = JSON.parse(undelegations.content).result;
                 delegations.unbonding = undelegations.entries.length;
                 delegations.unbondingCompletionTime = undelegations.entries[0].completion_time;
             };
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
@@ -178,14 +176,14 @@ Meteor.methods({
         return delegations
     },
 
-    'accounts.getAllDelegations'(address){
-        let url = LCD + '/staking/delegators/'+address+'/delegations';
+    'accounts.getAllDelegations'(address) {
+        let url = LCD + '/staking/delegators/' + address + '/delegations';
 
-        try{
+        try {
             let delegations = HTTP.get(url);
-            if (delegations.statusCode == 200){
+            if (delegations.statusCode == 200) {
                 delegations = JSON.parse(delegations.content).result;
-                if (delegations && delegations.length > 0){
+                if (delegations && delegations.length > 0) {
                     delegations.forEach((delegation, i) => {
                         if (delegations[i] && delegations[i].shares)
                             delegations[i].shares = parseFloat(delegations[i].shares);
@@ -194,28 +192,26 @@ Meteor.methods({
 
                 return delegations;
             };
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
     },
-    'accounts.getAllUnbondings'(address){
-        let url = LCD + '/staking/delegators/'+address+'/unbonding_delegations';
+    'accounts.getAllUnbondings'(address) {
+        let url = LCD + '/staking/delegators/' + address + '/unbonding_delegations';
 
-        try{
+        try {
             let unbondings = HTTP.get(url);
-            if (unbondings.statusCode == 200){
+            if (unbondings.statusCode == 200) {
                 unbondings = JSON.parse(unbondings.content).result;
                 return unbondings;
             };
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
     },
-    'accounts.getAllRedelegations'(address, validator){
+    'accounts.getAllRedelegations'(address, validator) {
         let url = `/staking/redelegations?delegator=${address}&validator_from=${validator}`;
         let result = fetchFromUrl(url);
         if (result && result.data) {
@@ -231,80 +227,72 @@ Meteor.methods({
         }
     },
 
-    'accounts.getRedelegations'(address){
+    'accounts.getRedelegations'(address) {
 
         let url = LCD + '/staking/redelegations?delegator' + address;
 
-        try{
+        try {
             let userRedelegations = HTTP.get(url);
-            if (userRedelegations.statusCode == 200){
+            if (userRedelegations.statusCode == 200) {
                 userRedelegations = JSON.parse(userRedelegations.content).result;
 
                 return userRedelegations;
             };
-        }
-        catch (e){
+        } catch (e) {
             console.log(url);
             console.log(e);
         }
     },
 
     //get BNB 
-    'accounts.getCDP': function(address){
-        
+    'accounts.getCDP': function (address) {
+
         let cdp = {}
 
         // get available atoms
         let url = LCD + '/cdp/cdps/denom/bnb'
-        try{
+        try {
             let result = HTTP.get(url);
-            if (result.statusCode == 200){
+            if (result.statusCode == 200) {
                 cdp = JSON.parse(result.content).result;
 
             }
-        }
-        catch (e){
+        } catch (e) {
             console.log(e)
         }
         return cdp
     },
 
-     //get Account CDP 
-     'accounts.getAccountCDP': function(address, collateral){
-        
+    //get Account CDP 
+    'accounts.getAccountCDP': function (address, collateral) {
+
         let cdp = {}
 
         // get available atoms
-        let url = LCD + '/cdp/cdps/cdp/'+address+'/'+ collateral 
-        try{
+        let url = LCD + '/cdp/cdps/cdp/' + address + '/' + collateral
+        try {
             let result = HTTP.get(url);
-            if (result.statusCode == 200){
+            if (result.statusCode == 200) {
                 cdp = JSON.parse(result.content).result;
             }
-        }
-        catch (e){
+        } catch (e) {
             console.log(e)
         }
         return cdp
     },
 
-        'cdp.getCDPList': function(){
-            this.unblock();
-            let url = LCD + '/cdp/cdps/denom/bnb';
-            try{
-                let result = HTTP.get(url);
-                if (result.statusCode == 200){
-                    let list = JSON.parse(result.content).result; 
-                       
-                    return list
-                    
-                }
-            }
-            catch (e){
-                console.log(e)
-            }
-        },
+    'cdp.getCDPList': function () {
+        this.unblock();
+        let url = LCD + '/cdp/cdps/denom/bnb';
+        try {
+            let result = HTTP.get(url);
+            if (result.statusCode == 200) {
+                let list = JSON.parse(result.content).result;
+                return list
 
-
-         
-}) 
+            }
+        } catch (e) {
+            console.log(e)
+        }
+    },
+})
--- imports/ui/cdp/List.jsx
+++ imports/ui/cdp/List.jsx
@@ -9,69 +9,60 @@ import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 const T = i18n.createComponent();
 
-
-
 const CDPRow = (props) => {
     return <tr>
         <th className="d-none d-sm-table-cell counter ">{(props.cdpList.cdp.id)}</th>
-            <td className="owner"><Link to={"/account/"+props.cdpList.cdp.owner}>{props.cdpList.cdp.owner}</Link></td>
-            <td className="collateral-deposited">{props.cdpList.collateral_value?
-             <div>{new Coin(props.cdpList.collateral_value.amount, props.cdpList.collateral_value.denom).toString(4)}</div>
-        :'0 usdx'}</td>
-        <td className="principal-drawn">{props.cdpList.cdp.principal?props.cdpList.cdp.principal.map((deposit, i) => {
-            return <div key={i}>{new Coin(deposit.amount, deposit.denom).toString()}</div>
-        }):'0 usdx'}</td>
-            <td className="value ">{numbro(parseFloat(props.cdpList.collateralization_ratio)).format('0.000000')}</td>
-            <td className="accumulated-fees">{props.cdpList.cdp.accumulated_fees?props.cdpList.cdp.accumulated_fees.map((deposit, i) => {
-            return <div key={i}>{new Coin(deposit.amount, deposit.denom).toString()}</div>
-        }):'0 usdx'}</td>
-            <td className="fees-updated"><TimeStamp time={props.cdpList.cdp.fees_updated}/></td>
-            
-         </tr>
+        <td className="owner"><Link to={"/account/"+props.cdpList.cdp.owner}>{props.cdpList.cdp.owner}</Link></td>
+        <td className="collateral-deposited">{props.cdpList.collateral_value?<div>{new Coin(props.cdpList.collateral_value.amount, props.cdpList.collateral_value.denom).toString(4)}</div>:'0 usdx'}</td>
+        <td className="principal-drawn">{props.cdpList.cdp.principal?<div>{new Coin(props.cdpList.cdp.principal.amount, props.cdpList.cdp.principal.denom).toString()}</div>:'0 usdx'}</td>
+        <td className="value ">{numbro(parseFloat(props.cdpList.collateralization_ratio)).format('0.000000')}</td>
+        <td className="accumulated-fees">{props.cdpList.cdp.accumulated_fees?<div>{new Coin(props.cdpList.cdp.accumulated_fees.amount, props.cdpList.cdp.accumulated_fees.denom).toString()}</div>:'0 usdx'}</td>
+        <td className="fees-updated"><TimeStamp time={props.cdpList.cdp.fees_updated}/></td>
+    </tr>
 }
 
 export default class List extends Component{
 
-        constructor(props){
-            super(props);
-            this.state = {
-                cdpList: [],
-                currentPage: 0,
-                pageSize: 15,
-                pagesCount: 0,
-            }
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            cdpList: [],
+            currentPage: 0,
+            pageSize: 15,
+            pagesCount: 0,
         }
-       
+    }
+    
+    componentDidMount(){
+        this.getCDPList();
+    }
+
     getCDPList = () => {
-            Meteor.call('cdp.getCDPList',  (error, result) => {
-                if (result && result > 0){
-                    this.setState({
-                       cdpList: result.map((cdpList, i) => {
+        Meteor.call('cdp.getCDPList',  (error, result) => {
+            if (result && result.length > 0){
+                this.setState({
+                    cdpList: result.map((cdpList, i) => {
                         return <CDPRow key={i} index={i} cdpList={cdpList} />
                     }),
-                    pagesCount: Math.ceil(this.state.cdpList.length / this.state.pageSize),
+                    pagesCount: Math.ceil(result.length / this.state.pageSize),
 
                 })
             }
-                else{
-                    this.setState({
-                        cdpList: undefined,
-                        pagesCount: 0,
-                
-                    })
-                }
+            else{
+                this.setState({
+                    cdpList: undefined,
+                    pagesCount: 0,
+            
+                })
             }
-        )} 
+        })
+    } 
 
     handleClick(e, index) {
-
         e.preventDefault();
-
         this.setState({
             currentPage: index
         });
-    
     }
 
     render(){
@@ -79,63 +70,51 @@ export default class List extends Component{
             return <Spinner type="grow" color="primary" />
         }
         else{
-            return (
-                 <div>
-                    {this.getCDPList()}
-      
+            return (<div>
                 <div className="pagination-wrapper">
                     <Table striped className="cdp-list" >
                         <thead>
                             <tr>
-                            <th className="d-none d-sm-table-cell counter "><i className="material-icons">sort</i> <T>cdp.cdpID</T></th>
+                                <th className="d-none d-sm-table-cell counter "><i className="material-icons">sort</i> <T>cdp.cdpID</T></th>
                                 <th className="d-none d-sm-table-cell owner"><i className="material-icons">account_circle</i> <span className="d-none d-sm-inline"><T>cdp.owner</T></span></th>
                                 <th className="collateral-deposited "><i className="material-icons">attach_money</i> <span className="d-none d-sm-inline"><T>cdp.collateralDeposited</T></span></th>
                                 <th className="principal-drawn"><i className="material-icons">attach_money</i> <span className="d-none d-sm-inline"><T>cdp.principal</T></span></th>
                                 <th className="value "><i className="material-icons">attach_money</i> <span className="d-none d-sm-inline"><T>cdp.collateralizationRatio</T></span></th>
                                 <th className="accumulated-fees"><i className="material-icons">attach_money</i> <span className="d-none d-sm-inline"><T>cdp.accumulatedFees</T></span></th>
                                 <th className="fees-updated"><i className="material-icons">update</i> <span className="d-none d-sm-inline"><T>cdp.cdpFeesUpdated</T> (UTC)</span></th>
-                               
                             </tr>
                         </thead>
                         <tbody>
                             {this.state.cdpList? this.state.cdpList.slice(this.state.currentPage * this.state.pageSize,(this.state.currentPage + 1) * this.state.pageSize) : null}
                         </tbody>
                     </Table>
-                    </div>
-                    <Pagination aria-label="Page navigation example" >
-          
+                </div>
+                <Pagination aria-label="Page navigation example" >
                     <PaginationItem disabled={this.state.currentPage <= 0}>
-                        
                         <PaginationLink
-                        onClick={e => this.handleClick(e, this.state.currentPage - 1)}
-                        previous
-                        href="#"
-                        
+                            onClick={e => this.handleClick(e, this.state.currentPage - 1)}
+                            previous
+                            href="#"                        
                         />
-                        
                     </PaginationItem>
                 
                     {[...Array(this.state.pagesCount)].map((page, i) => 
                         <PaginationItem active={i === this.state.currentPage} key={i}>
-                        <PaginationLink onClick={e => this.handleClick(e, i)} href="#">
-                            {i + 1}
-                        </PaginationLink>
+                            <PaginationLink onClick={e => this.handleClick(e, i)} href="#">
+                                {i + 1}
+                            </PaginationLink>
                         </PaginationItem>
                     )}
                 
-                    <PaginationItem disabled={this.state.currentPage >= this.state.pagesCount - 1}>
-                        
+                    <PaginationItem disabled={this.state.currentPage >= this.state.pagesCount - 1}>                        
                         <PaginationLink
-                        onClick={e => this.handleClick(e, currentPage + 1)}
-                        next
-                        href="#"
-                        />
-                        
+                            onClick={e => this.handleClick(e, currentPage + 1)}
+                            next
+                            href="#"
+                        />                        
                     </PaginationItem>
-                    
-                    </Pagination>
-            </div>
-            )
+                </Pagination>
+            </div>)
         }
     }
 }
