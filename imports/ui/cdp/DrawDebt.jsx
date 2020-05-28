e166d369bcdf312f61eaf7ef7d3bbd1ec2819b83
Kwun Yeung <kwun@forbole.com>
2020-05-28 22:44:49 +0800

    update DrawCDP component princpial object

--- imports/ui/cdp/DrawDebt.jsx
+++ imports/ui/cdp/DrawDebt.jsx
@@ -20,28 +20,28 @@ export default class DrawDebt extends Component {
 
     render(){
         if (this.props){
-        return <div><Button className="ledger-buttons-group my-2" color="primary" id="toggler_draw_debt" size="sm"><T>transactions.info</T>  </Button>
-         <UncontrolledCollapse toggler="#toggler_draw_debt">
-         <Table responsive>
-                <tbody>
-                    <tr>
-                        <th scope="row"><T>cdp.sender</T></th>
-                        <td>{this.props.sender ? <Account address={this.props.sender}/> : null}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row"><T>cdp.cdpDenom</T></th>
-                        <td>{this.props.cdp_denom? this.props.cdp_denom : null}</td>
-                    </tr>
-                    <tr>
-                        <th scope="row"><T>cdp.principal</T></th>
-                        <td>{this.props.principal?
-                        <span className={'coin'}>{new Coin((this.props.principal[0].amount), this.props.principal[0].denom).toString(4)}</span>:null}                        
-                        </td>
-                    </tr>
-                </tbody>
-                </Table>
-        </UncontrolledCollapse>
-        </div>   
+            return <div><Button className="ledger-buttons-group my-2" color="primary" id="toggler_draw_debt" size="sm"><T>transactions.info</T>  </Button>
+                <UncontrolledCollapse toggler="#toggler_draw_debt">
+                    <Table responsive>
+                        <tbody>
+                            <tr>
+                                <th scope="row"><T>cdp.sender</T></th>
+                                <td>{this.props.sender ? <Account address={this.props.sender}/> : null}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><T>cdp.cdpDenom</T></th>
+                                <td>{this.props.cdp_denom? this.props.cdp_denom : null}</td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><T>cdp.principal</T></th>
+                                <td>{this.props.principal?
+                                    <span className="coin">{(this.props.principal)?new Coin((this.props.principal.amount), this.props.principal.denom).toString(4):''}</span>:null}                        
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </UncontrolledCollapse>
+            </div>   
         }
         else{
             return <div />
@@ -52,5 +52,5 @@ export default class DrawDebt extends Component {
 DrawDebt.propTypes = {
     sender: PropTypes.string.isRequired,
     cdp_denom: PropTypes.string.isRequired,
-    principal: PropTypes.array.isRequired
+    principal: PropTypes.object.isRequired
 }
