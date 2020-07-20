fad6f23d36a816d66e7bf25836bea8c74db02b4e
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2020-07-20 13:13:07 +0800

    Updated the message type for Validator Commission & Rewards Withdraw

--- imports/ui/ledger/LedgerActions.jsx
+++ imports/ui/ledger/LedgerActions.jsx
@@ -924,7 +924,7 @@ class WithdrawButton extends LedgerButton {
             if (res) {
                 if (this.props.address) {
                     res.value.msg.push({
-                        type: 'cosmos-sdk/MsgWithdrawValidatorCommission',
+                        type: 'cosmos-sdk/MsgWithdrawValidatorRewardsAll',
                         value: { validator_address: this.props.address }
                     })
                 }
