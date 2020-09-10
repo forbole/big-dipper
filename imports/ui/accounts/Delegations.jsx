1af8df8ae24b96a7c5261fadee1782cb18395a92
Kwun Yeung <kwun@forbole.com>
2020-09-11 00:53:32 +0800

    fix account delegation validator name

--- imports/ui/accounts/Delegations.jsx
+++ imports/ui/accounts/Delegations.jsx
@@ -34,7 +34,7 @@ export default class AccountDelegations extends Component{
                             let reward = this.props.rewardsForEachDel[d.validator_address];
                             rewardDenom =(reward)?reward.find(({denom}) => denom === denomType): null;
                             return <Row key={i} className="delegation-info">
-                                <Col xs={7} md={4} className="text-nowrap overflow-auto"><AccountTooltip address={d.validator_address} /></Col>
+                                <Col xs={7} md={4} className="text-nowrap overflow-auto"><AccountTooltip address={d.delegation.validator_address} /></Col>
                                 <Col xs={2} md={5} className="overflow-auto">{new Coin(d.balance.amount, denomType).stakeString()}</Col>
                                 <Col xs={3} md={3}>{rewardDenom?new Coin(rewardDenom.amount, rewardDenom.denom).toString(4):'No rewards '} </Col>
                             </Row>
