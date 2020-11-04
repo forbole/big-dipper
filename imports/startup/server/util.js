50ab3fdf5ef51be084eb36534ae7efd36176a4e9
Kwun Yeung <kwun@forbole.com>
2020-11-04 19:43:30 +0800

    add versioning (#423)
    * add versioning
    
    * update changelog

--- imports/startup/server/util.js
+++ imports/startup/server/util.js
@@ -38,5 +38,9 @@ Meteor.methods({
             let page = cheerio.load(teamPage.content);
             return page(".kb-main-card img").attr('src');
         }
+    },
+    getVersion: function(){
+        const version = Assets.getText('version');
+        return version ? version : 'beta'
     }
 })
