860ead8ae80e1cb6172796af776a47814ecc880f
Kwun Yeung <kwun@forbole.com>
2020-11-29 01:43:21 +0800

    add try catch on pubkey conversion

--- imports/startup/server/util.js
+++ imports/startup/server/util.js
@@ -17,49 +17,61 @@ Meteor.methods({
     pubkeyToBech32: function(pubkey, prefix) {
         let buffer;
 
-        if (pubkey.type.indexOf("PubKeyEd25519") > 0){
+        try {
+            if (pubkey.type.indexOf("PubKeyEd25519") > 0){
             // '1624DE6420' is ed25519 pubkey prefix
-            let pubkeyAminoPrefix = Buffer.from('1624DE6420', 'hex');
-            buffer = Buffer.alloc(37);
+                let pubkeyAminoPrefix = Buffer.from('1624DE6420', 'hex');
+                buffer = Buffer.alloc(37);
         
-            pubkeyAminoPrefix.copy(buffer, 0)
-            Buffer.from(pubkey.value, 'base64').copy(buffer, pubkeyAminoPrefix.length)
-        }
-        else if (pubkey.type.indexOf("PubKeySecp256k1") > 0){
+                pubkeyAminoPrefix.copy(buffer, 0)
+                Buffer.from(pubkey.value, 'base64').copy(buffer, pubkeyAminoPrefix.length)
+            }
+            else if (pubkey.type.indexOf("PubKeySecp256k1") > 0){
             // 'EB5AE98721' is secp256k1 pubkey prefix
-            let pubkeyAminoPrefix = Buffer.from('EB5AE98721', 'hex');
-            buffer = Buffer.alloc(38);
+                let pubkeyAminoPrefix = Buffer.from('EB5AE98721', 'hex');
+                buffer = Buffer.alloc(38);
     
-            pubkeyAminoPrefix.copy(buffer, 0)
-            Buffer.from(pubkey.value, 'base64').copy(buffer, pubkeyAminoPrefix.length)
-        }
-        else {
-            console.log("Pubkey type not supported.");
-            return false;
-        }
+                pubkeyAminoPrefix.copy(buffer, 0)
+                Buffer.from(pubkey.value, 'base64').copy(buffer, pubkeyAminoPrefix.length)
+            }
+            else {
+                console.log("Pubkey type not supported.");
+                return false;
+            }
 
-        return bech32.encode(prefix, bech32.toWords(buffer))
+            return bech32.encode(prefix, bech32.toWords(buffer))
+        }
+        catch (e){
+            console.log("Error converting from pubkey to bech32: %o\n %o", pubkey, e);
+            return false
+        }
     },
     bech32ToPubkey: function(pubkey, type) {
         // type can only be either 'tendermint/PubKeySecp256k1' or 'tendermint/PubKeyEd25519'
         let pubkeyAminoPrefix, buffer;
 
-        if (type.indexOf("PubKeyEd25519") > 0){
+        try {
+            if (type.indexOf("PubKeyEd25519") > 0){
             // '1624DE6420' is ed25519 pubkey prefix
-            pubkeyAminoPrefix = Buffer.from('1624DE6420', 'hex')
-            buffer = Buffer.from(bech32.fromWords(bech32.decode(pubkey).words));
-        }
-        else if (type.indexOf("PubKeySecp256k1") > 0){
+                pubkeyAminoPrefix = Buffer.from('1624DE6420', 'hex')
+                buffer = Buffer.from(bech32.fromWords(bech32.decode(pubkey).words));
+            }
+            else if (type.indexOf("PubKeySecp256k1") > 0){
             // 'EB5AE98721' is secp256k1 pubkey prefix
-            pubkeyAminoPrefix = Buffer.from('EB5AE98721', 'hex')
-            buffer = Buffer.from(bech32.fromWords(bech32.decode(pubkey).words));
-        }
-        else {
-            console.log("Pubkey type not supported.");
-            return false;
-        }
+                pubkeyAminoPrefix = Buffer.from('EB5AE98721', 'hex')
+                buffer = Buffer.from(bech32.fromWords(bech32.decode(pubkey).words));
+            }
+            else {
+                console.log("Pubkey type not supported.");
+                return false;
+            }
         
-        return buffer.slice(pubkeyAminoPrefix.length).toString('base64');
+            return buffer.slice(pubkeyAminoPrefix.length).toString('base64');
+        }
+        catch (e){
+            console.log("Error converting from bech32 to pubkey: %o\n %o", pubkey, e);
+            return false
+        }
     },
     getDelegator: function(operatorAddr){
         let address = bech32.decode(operatorAddr);
