0d41d3c4f00f44154c4224b77065e8a261d9ce85
Kwun Yeung <kwun@forbole.com>
2021-02-17 14:32:33 +0000

    fix transaction row

--- imports/ui/transactions/Transaction.jsx
+++ imports/ui/transactions/Transaction.jsx
@@ -58,13 +58,13 @@ export default class Transaction extends Component{
                                     {tx.block()?<span> <TimeStamp time={tx.block().time}/></span>:null}
                                 </Col>
                                 <Col md={4} className="label"><T>transactions.fee</T></Col>
-                                <Col md={8} className="value">{(tx.tx.value.fee.amount.length > 0)?tx.tx.value.fee.amount.map((fee,i) => {
+                                <Col md={8} className="value">{(tx.tx.auth_info.fee.amount.length > 0)?tx.tx.auth_info.fee.amount.map((fee,i) => {
                                     return <span className="text-nowrap" key={i}> {((fee.amount/Meteor.settings.public.stakingFraction)>=1)?(new Coin(parseFloat(fee.amount), fee.denom)).stakeString():(new Coin(parseFloat(fee.amount), fee.denom)).mintString()} </span>
                                 }):<span><T>transactions.noFee</T></span>}</Col>
                                 <Col md={4} className="label"><T>transactions.gasUsedWanted</T></Col>
-                                <Col md={8} className="value">{numbro(tx.gas_used).format("0,0")} / {numbro(tx.gas_wanted).format("0,0")}</Col>
+                                <Col md={8} className="value">{numbro(tx.tx_response.gas_used).format("0,0")} / {numbro(tx.tx_response.gas_wanted).format("0,0")}</Col>
                                 <Col md={4} className="label"><T>transactions.memo</T></Col>
-                                <Col md={8} className="value"><Markdown markup={ tx.tx.body.mmo } /></Col>
+                                <Col md={8} className="value"><Markdown markup={ tx.tx.body.memo } /></Col>
                               
                             </Row>
                         </CardBody>
--- imports/ui/transactions/TransactionRow.jsx
+++ imports/ui/transactions/TransactionRow.jsx
@@ -18,19 +18,19 @@ export const TransactionRow = (props) => {
     let tx = props.tx;
      
     return <SentryBoundary><Row className={(tx.code)?"tx-info invalid":"tx-info"}>
-        <Col xs={12} lg={7} className="activity">{(tx.tx.value.msg && tx.tx.value.msg.length >0)?tx.tx.value.msg.map((msg,i) => {
-            return <Card body key={i}><Activities msg={msg} invalid={(!!tx.code)} events={(tx.logs&&tx.logs[i])?tx.logs[i].events:null} /></Card>
+        <Col xs={12} lg={7} className="activity">{(tx.tx.body.messages && tx.tx.body.messages.length >0)?tx.tx.body.messages.map((msg,i) => {
+            return <Card body key={i}><Activities msg={msg} invalid={(!!tx.tx_response.code)} events={(tx.tx_response.logs&&tx.tx_response.logs[i])?tx.tx_response.logs[i].events:null} /></Card>
         }):''}</Col>
         <Col xs={(!props.blockList)?{size:6,order:"last"}:{size:12,order:"last"}} md={(!props.blockList)?{size:3, order: "last"}:{size:7, order: "last"}} lg={(!props.blockList)?{size:1,order:"last"}:{size:2,order:"last"}} className="text-truncate"><i className="fas fa-hashtag d-lg-none"></i> <Link to={"/transactions/"+tx.txhash}>{tx.txhash}</Link></Col>
-        <Col xs={6} md={9} lg={{size:2,order:"last"}} className="text-nowrap"><i className="material-icons">schedule</i> <span>{tx.block()?<TimeAgo time={tx.block().time} />:''}</span>{(tx.tx.value.memo && tx.tx.value.memo != "")?<span>
+        <Col xs={6} md={9} lg={{size:2,order:"last"}} className="text-nowrap"><i className="material-icons">schedule</i> <span>{tx.block()?<TimeAgo time={tx.block().time} />:''}</span>{(tx.tx.body.memo && tx.tx.body.memo != "")?<span>
             <i className="material-icons ml-2 memo-button" id={"memo-"+tx.txhash}>message</i>
             <UncontrolledPopover trigger="legacy" placement="top-start" target={"memo-"+tx.txhash}>
-                <PopoverBody><Markdown markup={tx.tx.value.memo} /></PopoverBody>
+                <PopoverBody><Markdown markup={tx.tx.body.memo} /></PopoverBody>
             </UncontrolledPopover>
         </span>:""}</Col>
         {(!props.blockList)?<Col xs={4} md={2} lg={1}><i className="fas fa-database d-lg-none"></i> <Link to={"/blocks/"+tx.height}>{numbro(tx.height).format("0,0")}</Link></Col>:''}
         <Col xs={(!props.blockList)?2:4} md={1}>{(!tx.code)?<TxIcon valid />:<TxIcon />}</Col>
-        <Col xs={(!props.blockList)?6:8} md={(!props.blockList)?9:4} lg={2} className="fee"><i className="material-icons d-lg-none">monetization_on</i> {(tx.tx.value.fee.amount.length > 0)?tx.tx.value.fee.amount.map((fee,i) => {
+        <Col xs={(!props.blockList)?6:8} md={(!props.blockList)?9:4} lg={2} className="fee"><i className="material-icons d-lg-none">monetization_on</i> {(tx.tx.auth_info.fee.amount.length > 0)?tx.tx.auth_info.fee.amount.map((fee,i) => {
             return <span className="text-nowrap" key={i}>{(new Coin(parseFloat(fee.amount), (fee)?fee.denom:null)).stakeString()}</span>
         }):<span>No fee</span>}</Col>
         {(tx.code)?<Col xs={{size:12, order:"last"}} className="error">
