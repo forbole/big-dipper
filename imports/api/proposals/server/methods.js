190e0cf60b49ead1f7e1158c6bea9f48b67f804c
Kwun Yeung <kwun@forbole.com>
2021-03-10 17:39:44 +0000

    get correct votes and tally results

--- imports/api/proposals/server/methods.js
+++ imports/api/proposals/server/methods.js
@@ -74,25 +74,25 @@ Meteor.methods({
                     let url = "";
                     try{
                         // get proposal deposits
-                        url = API + '/cosmos/gov/v1beta1/proposals/'+proposals[i].proposalId+'/deposits';
+                        url = API + '/cosmos/gov/v1beta1/proposals/'+proposals[i].proposalId+'/deposits?pagination.limit=2000&pagination.count_total=true';
                         let response = HTTP.get(url);
                         let proposal = {proposalId: proposals[i].proposalId};
                         if (response.statusCode == 200){
-                            let deposits = JSON.parse(response.content).result;
+                            let deposits = JSON.parse(response.content).deposits;
                             proposal.deposits = deposits;
                         }
 
-                        url = API + '/cosmos/gov/v1beta1/proposals/'+proposals[i].proposalId+'/votes';
+                        url = API + '/cosmos/gov/v1beta1/proposals/'+proposals[i].proposalId+'/votes?pagination.limit=2000&pagination.count_total=true';
                         response = HTTP.get(url);
                         if (response.statusCode == 200){
-                            let votes = JSON.parse(response.content).result;
+                            let votes = JSON.parse(response.content).votes;
                             proposal.votes = getVoteDetail(votes);
                         }
 
                         url = API + '/cosmos/gov/v1beta1/proposals/'+proposals[i].proposalId+'/tally';
                         response = HTTP.get(url);
                         if (response.statusCode == 200){
-                            let tally = JSON.parse(response.content).result;
+                            let tally = JSON.parse(response.content).tally;
                             proposal.tally = tally;
                         }
 
@@ -118,41 +118,41 @@ const getVoteDetail = (votes) => {
     let voters = votes.map((vote) => vote.voter);
     let votingPowerMap = {};
     let validatorAddressMap = {};
-    Validators.find({delegatorAddress: {$in: voters}}).forEach((validator) => {
-        votingPowerMap[validator.delegatorAddress] = {
+    Validators.find({delegator_address: {$in: voters}}).forEach((validator) => {
+        votingPowerMap[validator.delegator_address] = {
             moniker: validator.description.moniker,
             address: validator.address,
             tokens: parseFloat(validator.tokens),
-            delegatorShares: parseFloat(validator.delegatorShares),
-            deductedShares: parseFloat(validator.delegatorShares)
+            delegatorShares: parseFloat(validator.delegator_shares),
+            deductedShares: parseFloat(validator.delegator_shares)
         }
-        validatorAddressMap[validator.operatorAddress] = validator.delegatorAddress;
+        validatorAddressMap[validator.operator_address] = validator.delegator_address;
     });
     voters.forEach((voter) => {
         if (!votingPowerMap[voter]) {
             // voter is not a validator
-            let url = `${API}/cosmos/staking/v1beta1/delegators/${voter}/delegations`;
+            let url = `${API}/cosmos/staking/v1beta1/delegations/${voter}`;
             let delegations;
             let votingPower = 0;
             try{
                 let response = HTTP.get(url);
                 if (response.statusCode == 200){
-                    delegations = JSON.parse(response.content).result;
+                    delegations = JSON.parse(response.content).delegations_response;
                     if (delegations && delegations.length > 0) {
                         delegations.forEach((delegation) => {
-                            let shares = parseFloat(delegation.shares);
-                            if (validatorAddressMap[delegation.validator_address]) {
+                            let shares = parseFloat(delegation.delegation.shares);
+                            if (validatorAddressMap[delegation.delegation.validator_address]) {
                                 // deduct delegated shareds from validator if a delegator votes
                                 let validator = votingPowerMap[validatorAddressMap[delegation.validator_address]];
                                 validator.deductedShares -= shares;
                                 if (validator.delegatorShares != 0){ // avoiding division by zero
-                                    votingPower += (shares/validator.delegatorShares) * validator.tokens;
+                                    votingPower += (shares/validator.delegatorShares) * validator.tokens / Meteor.settings.public.powerReduction;
                                 }
 
                             } else {
                                 let validator = Validators.findOne({operatorAddress: delegation.validator_address});
                                 if (validator && validator.delegatorShares != 0){ // avoiding division by zero
-                                    votingPower += (shares/parseFloat(validator.delegatorShares)) * parseFloat(validator.tokens);
+                                    votingPower += (shares/parseFloat(validator.delegatorShares)) * parseFloat(validator.tokens) / Meteor.settings.public.powerReduction;
                                 }
                             }
                         });
@@ -171,7 +171,7 @@ const getVoteDetail = (votes) => {
         let votingPower = voter.votingPower;
         if (votingPower == undefined) {
             // voter is a validator
-            votingPower = voter.delegatorShares?((voter.deductedShares/voter.delegatorShares) * voter.tokens):0;
+            votingPower = voter.delegatorShares?((voter.deductedShares/voter.delegatorShares) * voter.tokens / Meteor.settings.public.powerReduction):0;
         }
         return {...vote, votingPower};
     });
