cd862d8ecdb75e43ae733d4b14a18174670cd921
Magic Cat <pusz.monika@gmail.com>
2021-05-04 17:14:46 +0800

    Added ledger address list with radio selection and button to load more addresses

--- imports/ui/ledger/LedgerModal.jsx
+++ imports/ui/ledger/LedgerModal.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Button, Spinner, TabContent, TabPane, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter, UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap';
-import {Ledger} from './ledger.js';
+import { Button, Spinner, TabContent, TabPane, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Ledger } from './ledger.js';
 import i18n from 'meteor/universe:i18n';
 
 const T = i18n.createComponent();
@@ -11,9 +11,12 @@ class LedgerModal extends React.Component {
         this.state = {
             loading: false,
             activeTab: '1',
-            transportBLE: localStorage.getItem(BLELEDGERCONNECTION) ?? false
+            transportBLE: localStorage.getItem(BLELEDGERCONNECTION) ?? false,
+            addressHasBeenSelected: false,
+            fromAccountIndex: 0,
+            toAccountIndex: 10
         };
-        this.ledger = new Ledger({testModeAllowed: false});
+        this.ledger = new Ledger({ testModeAllowed: false });
     }
 
     autoOpenModal = () => {
@@ -32,6 +35,9 @@ class LedgerModal extends React.Component {
         let bleTransport = this.state.transportBLE
         if (bleTransport != prevState.transportBLE) {
             this.tryConnect();
+        }
+        if(this.state.toAccountIndex != prevState.toAccountIndex){
+            this.renderDropDown()
         }
     }
 
@@ -82,27 +88,33 @@ class LedgerModal extends React.Component {
                 activeTab: '2'
             });
         }
-      
-        
     }
 
-    handleAddressSwitch = (address, index, e) => {
+    handleAddressSwitch = (e) => {
         e.preventDefault();
-        localStorage.setItem('addressIndex', index)
+        localStorage.setItem('addressIndex', e.target.index)
         this.setState({
-            currentUserAddress: address,
+            currentUserAddress: e.target.address,
+            accountIndex: e.target.index,
             addressHasBeenSelected: true
+        })
+    }
+
+    loadMoreAddresses = (e) => {
+        this.setState({
+            fromAccountIndex: this.state.toAccountIndex,
+            toAccountIndex: this.state.toAccountIndex + 10
         })
     }
 
     async renderDropDown() {
         this.setState({ loadingAccountsList: true })
         let accounts = [];
-        for(let c = 0; c < 10; c++){
+        for (let c = this.state.fromAccountIndex; c < this.state.toAccountIndex; c++){
             // eslint-disable-next-line no-await-in-loop
             accounts[c] = await this.ledger.getLedgerAddresses(c, this.state.transportBLE)
         }
-        if(accounts.length === 10){
+        if (accounts.length === this.state.toAccountIndex){
             this.setState({ loadingAccountsList: false, accountsList: accounts, addressesLoaded: true })
         }
         return accounts
@@ -111,7 +123,7 @@ class LedgerModal extends React.Component {
 
 
     trySignIn = () => {
-        this.setState({ loading: true, errorMessage: ''})
+        this.setState({ loading: true, errorMessage: '' })
         this.ledger.confirmLedgerAddress(this.state.transportBLE).then((res) => {
             localStorage.setItem(CURRENTUSERADDR, this.state.address);
             localStorage.setItem(CURRENTUSERPUBKEY, this.state.pubKey);
@@ -166,22 +178,15 @@ class LedgerModal extends React.Component {
                         </TabPane>
                         <TabPane tabId="2">
                             {this.state.currentUser?<span>You are currently logged in as <strong className="text-primary d-block">{this.state.currentUser}.</strong></span>:null}
-                            {this.state.loadingAccountsList ? <Spinner type="grow" color="primary" /> :
-                            <>
-                            <span className="text-primary text-center mb-3 d-block">Select address to log in with from the list below: </span>
-                                    <UncontrolledDropdown direction='down' size="md" className='ledger-accounts-dropdown text-center'>
-                                        <DropdownToggle caret >
-                                            {this.state.currentUserAddress ? <span className="text-lowercase">{this.state.currentUserAddress}</span> : <span>Select address</span>}
-                                        </DropdownToggle>
-                                        <DropdownMenu>
-                                            {
-                                                this.state.accountsList ? this.state.accountsList?.map((option, k) => (
-                                                    <DropdownItem key={k} onClick={(e) => this.handleAddressSwitch(option.address,k, e)}>{option.address.toLowerCase()}</DropdownItem>
-                                                )) : null}
-                                        </DropdownMenu>
-                                    </UncontrolledDropdown> 
-                                </>}
                             {/* <T>accounts.toLoginAs</T> <strong className="text-primary d-block">{this.state.address}</strong><T>accounts.pleaseAccept</T> */}
+                            {this.state.loadingAccountsList ? <Spinner type="grow" color="primary" /> :
+                                <>
+                                    <span className="text-primary text-center mb-3 d-block">Select address to log in with from the list below: </span>
+                                    {this.state.accountsList?.map((option, k) => (
+                                        <div className="mb-1 ml-1" key={k}> <span className="font-weight-bold mr-1">{`${k}.`}</span><input type="radio" name="ledger-address-selection" className="ml-1" index={k} address={option.address.toLowerCase()} onChange={this.handleAddressSwitch} /> {`${option.address.toLowerCase()}`} </div>
+                                    ))}
+                                    <div className="text-center mt-3"> <Button outline color="danger" size="sm" onClick={this.loadMoreAddresses}>Show more addresses...</Button> </div>
+                                </>}
                         </TabPane>
                     </TabContent>
                     {this.state.loading?<Spinner type="grow" color="primary" />:''}
