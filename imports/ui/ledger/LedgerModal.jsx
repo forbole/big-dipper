b3144e593633c79f92c4127455dacee9e5624fe4
Magic Cat <pusz.monika@gmail.com>
2021-05-05 17:27:00 +0800

    Add dividers and default account message

--- imports/ui/ledger/LedgerModal.jsx
+++ imports/ui/ledger/LedgerModal.jsx
@@ -179,8 +179,8 @@ class LedgerModal extends React.Component {
                         <TabPane tabId="1">
                             <T _purify={false} network={Meteor.settings.public.ledger.appName} version={Meteor.settings.public.ledger.appVersion}>accounts.signInWarning</T>
                             <div className="d-flex justify-content-center">
-                                <Button color="secondary" value="usb" onClick={this.connectionSelection} className="mt-3 mr-4"><span><img src="/img/usb.svg" alt="USB" style={{height: "25px"}}/><T>USB</T></span></Button>
-                                <Button color="secondary" value="bluetooth" onClick={this.connectionSelection} className="mt-3 "><span><img src="/img/bluetooth.svg" alt="Bluetooth" style={{ height: "25px" }} /><T>Bluetooth</T></span></Button>
+                                <Button color="secondary" value="usb" onClick={this.connectionSelection} className="mt-3 mr-4 btn-ledger-login"><span><img src="/img/usb.svg" alt="USB" style={{height: "25px"}}/><T>USB</T></span></Button>
+                                <Button color="secondary" value="bluetooth" onClick={this.connectionSelection} className="mt-3 btn-ledger-login"><span><img src="/img/bluetooth.svg" alt="Bluetooth" style={{ height: "25px" }} /><T>Bluetooth</T></span></Button>
                             </div>
                             <h6 className="error-message text-center mt-3"><T>accounts.BLESupport</T></h6>
                         </TabPane>
@@ -189,14 +189,15 @@ class LedgerModal extends React.Component {
                             {/* <T>accounts.toLoginAs</T> <strong className="text-primary d-block">{this.state.address}</strong><T>accounts.pleaseAccept</T> */}
                             {this.state.loadingAccountsList ? <Spinner type="grow" color="primary" /> :
                                 <>
-                                    <span className="text-primary text-center mb-3 d-block">Select address to log in with from the list below: </span>
+                                    <span className="text-primary text-center mb-1 d-block">Select address to log in with from the list below: </span>
+                                    {!this.state.loadingAccountsList ? <h6 className="mt-1 mb-3 text-center">Your default address is account 0.</h6> : null}
                                     {this.state.accountsList?.map((option, k) => (
-                                        <div className="mb-1 ml-1" key={k}> <span className="font-weight-bold mr-1">{`${k}.`}</span><input type="radio" name="ledger-address-selection" className="ml-1 mr-1" onChange={(e) => this.handleAddressSwitch(e, k, option.address.toLowerCase())} />{`${option.address.toLowerCase()}`} </div>
+                                        <div className={k > 0 ? "mb-2 pt-2 border-top" : "mb-2"} key={k}><input type="radio" name="ledger-address-selection" className="ml-1 mr-1" onChange={(e) => this.handleAddressSwitch(e, k, option.address.toLowerCase())} /> <span className="font-weight-bold mr-1">{`${k}.`}</span>{`${option.address.toLowerCase()}`} </div>
                                     ))}
                                     <div className="text-center mt-3"> 
                                         {this.state.fromAccountIndex != 0 ? 
-                                            <Button outline color="danger" size="sm" onClick={this.loadPreviousAddresses} className="mr-4 text-capitalize">Load previous</Button> : null }
-                                        <Button outline color="danger" size="sm" onClick={this.loadNextAddresses} className="text-capitalize">Load more</Button> </div>
+                                            <Button outline color="danger" size="md" onClick={this.loadPreviousAddresses} className="mr-4 text-capitalize">Back</Button> : null }
+                                        <Button outline color="danger" size="md" onClick={this.loadNextAddresses} className="text-capitalize">Next</Button> </div>
                                 </>}
                         </TabPane>
                     </TabContent>
