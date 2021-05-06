645a8be926cbe3bde1491be1201d65ec53a6f166
Magic Cat <pusz.monika@gmail.com>
2021-05-06 16:33:19 +0800

    Updated i18n

--- imports/ui/ledger/LedgerModal.jsx
+++ imports/ui/ledger/LedgerModal.jsx
@@ -14,15 +14,14 @@ class LedgerModal extends React.Component {
             activeTab: '1',
             transportBLE: localStorage.getItem(BLELEDGERCONNECTION) ?? false,
             addressHasBeenSelected: false,
-            fromAccountIndex: 0,
-            toAccountIndex: 10
+            accountIndexStart: 0,
+            accountIndexEnd: 10
         };
         this.ledger = new Ledger({ testModeAllowed: false });
     }
 
     autoOpenModal = () => {
         if (!this.props.isOpen && this.props.handleLoginConfirmed) {
-            // this.tryConnect(5000);
             this.props.toggle(true);
         }
     }
@@ -37,8 +36,8 @@ class LedgerModal extends React.Component {
         if (bleTransport != prevState.transportBLE) {
             this.tryConnect();
         }
-        if(this.state.toAccountIndex != prevState.toAccountIndex){
-            this.renderDropDown()
+        if(this.state.accountIndexEnd != prevState.accountIndexEnd){
+            this.generateLedgerAddresses()
         }
     }
 
@@ -83,7 +82,7 @@ class LedgerModal extends React.Component {
             });
         }
         else{
-            this.renderDropDown();
+            this.generateLedgerAddresses();
             this.setState({
                 errorMessage: '',
                 loading: false,
@@ -104,27 +103,27 @@ class LedgerModal extends React.Component {
 
     loadNextAddresses = (e) => {
         this.setState({
-            fromAccountIndex: this.state.toAccountIndex,
-            toAccountIndex: this.state.toAccountIndex + 10
+            accountIndexStart: this.state.accountIndexEnd,
+            accountIndexEnd: this.state.accountIndexEnd + 10
         })
     }
 
     loadPreviousAddresses = (e) => {
         this.setState({
-            fromAccountIndex: this.state.fromAccountIndex - 10,
-            toAccountIndex: this.state.toAccountIndex - 10
+            accountIndexStart: this.state.accountIndexStart - 10,
+            accountIndexEnd: this.state.accountIndexEnd - 10
         })
     }
 
-    async renderDropDown() {
-        this.setState({ loadingAccountsList: true })
+    async generateLedgerAddresses() {
+        this.setState({ loadingAddressList: true })
         let accounts = [];
-        for (let c = this.state.fromAccountIndex; c < this.state.toAccountIndex; c++){
+        for (let c = this.state.accountIndexStart; c < this.state.accountIndexEnd; c++){
             // eslint-disable-next-line no-await-in-loop
             accounts[c] = await this.ledger.getLedgerAddresses(c, this.state.transportBLE)
         }
-        if (accounts.length === this.state.toAccountIndex){
-            this.setState({ loadingAccountsList: false, accountsList: accounts, addressesLoaded: true })
+        if (accounts.length === this.state.accountIndexEnd){
+            this.setState({ loadingAddressList: false, accountsList: accounts, addressesLoaded: true })
         }
         return accounts
     }
@@ -167,7 +166,6 @@ class LedgerModal extends React.Component {
             address: null,
             activeTab: '1'
         })
-        // localStorage.removeItem('addressIndex')
         this.props.toggle(false)
     }
 
@@ -188,17 +186,17 @@ class LedgerModal extends React.Component {
                         <TabPane tabId="2">
                             {this.state.currentUser?<span>You are currently logged in as <strong className="text-primary d-block">{this.state.currentUser}.</strong></span>:null}
                             {/* <T>accounts.toLoginAs</T> <strong className="text-primary d-block">{this.state.address}</strong><T>accounts.pleaseAccept</T> */}
-                            {this.state.loadingAccountsList ? <div className="d-block"> <h4 className="text-primary ml-2"> Generating accounts</h4>  <Loader /></div>:
+                            {this.state.loadingAddressList ? <div className="d-block"> <h4 className="text-primary ml-2"> <T>common.generatingAddresses</T></h4>  <Loader /></div>:
                                 <>
-                                    <span className="text-primary text-center mb-1 d-block">Select address to log in with from the list below: </span>
-                                    {!this.state.loadingAccountsList ? <h6 className="mt-1 mb-3 text-center">Your default address is account 0.</h6> : null}
+                                    <span className="text-primary text-center mb-1 d-block"> <T>common.selectAddress</T> </span>
+                                    {!this.state.loadingAddressList ? <h6 className="mt-1 mb-3 text-center"><T>common.defaultAddressMessage</T></h6> : null}
                                     {this.state.accountsList?.map((option, k) => (
                                         <div className={k > 0 ? "mb-2 pt-2 border-top" : "mb-2"} key={k}><input type="radio" name="ledger-address-selection" className="ml-1 mr-1" onChange={(e) => this.handleAddressSwitch(e, k, option.address.toLowerCase())} /> <span className="font-weight-bold mr-1">{`${k}.`}</span>{`${option.address.toLowerCase()}`} </div>
                                     ))}
                                     <div className="text-center mt-3"> 
-                                        {this.state.fromAccountIndex != 0 ? 
-                                            <Button outline color="danger" size="md" onClick={this.loadPreviousAddresses} className="mr-4 text-capitalize">Back</Button> : null }
-                                        <Button outline color="danger" size="md" onClick={this.loadNextAddresses} className="text-capitalize">Next</Button> </div>
+                                        {this.state.accountIndexStart != 0 ? 
+                                            <Button outline color="danger" size="md" onClick={this.loadPreviousAddresses} className="mr-4 text-capitalize"><T>common.back</T></Button> : null }
+                                        <Button outline color="danger" size="md" onClick={this.loadNextAddresses} className="text-capitalize"><T>common.next</T></Button> </div>
                                 </>}
                         </TabPane>
                     </TabContent>
