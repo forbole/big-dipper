ae95bfbb5ec2a181d5b7e568eb1ccc7ec0791de2
Magic Cat <pusz.monika@gmail.com>
2021-05-07 16:26:35 +0800

    Updated coins.js

--- both/utils/coins.js
+++ both/utils/coins.js
@@ -52,28 +52,20 @@ get stakingAmount () {
 toString (precision) {
     // default to display in mint denom if it has more than 4 decimal places
     let minStake = Coin.StakingCoin.fraction/(precision?(10 ** precision):10000)
-    if (this.amount < minStake) {
-        return `${numbro(this.amount).format('0,0.0000' )} ${this._coin.denom}`;
-    } else {
+    if (this.amount === 0) {
+        return `0 ${this._coin.displayName}`
+    }
+    else if (this.amount < minStake) {
+        return `${numbro(this.amount).format('0,0.000000' )} ${this._coin.denom}`;
+    }
+    else if (!this._coin.displayName){
+        return `${this.stakingAmount ?? 0} ${Coin.StakingCoin.displayName}`
+    }
+    else if (this.amount % 1 === 0){
+        return `${this.stakingAmount} ${this._coin.displayName}`
+    }
+    else {
         return `${precision?numbro(this.stakingAmount).format('0,0.' + '0'.repeat(precision)):autoformat(this.stakingAmount)} ${this._coin.displayName}`
     }
-}
-
-mintString (formatter) {
-    let amount = this.amount
-    if (formatter) {
-        amount = numbro(amount).format(formatter)
-    }
-
-    let denom = (this._coin == "")?Coin.StakingCoin.displayName:this._coin.denom;
-    return `${amount} ${denom}`;
-}
-
-stakeString (formatter) {
-    let amount = this.stakingAmount
-    if (formatter) {
-        amount = numbro(amount).format(formatter)
-    }
-    return `${amount} ${Coin.StakingCoin.displayName}`;
 }
 }
