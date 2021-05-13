7462926e28a028c38be1877ee673aeddbcdec9c5
Magic Cat <pusz.monika@gmail.com>
2021-05-13 17:05:49 +0800

    Update delegations methods.js

--- imports/api/delegations/server/methods.js
+++ imports/api/delegations/server/methods.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-loop-func */
 import { Meteor } from 'meteor/meteor';
 import { Delegations } from '../delegations.js';
 import { Validators } from '../../validators/validators.js';
+import fetch from 'node-fetch'
+
 
 Meteor.methods({
     'delegations.getDelegations': async function(){
@@ -11,21 +15,21 @@ Meteor.methods({
         for (v in validators){
             if (validators[v].operator_address){
                 let url = API + '/cosmos/staking/v1beta1/validators/'+validators[v].operatorAddress+"/delegations";
-                try{
-                    let response = HTTP.get(url);
-                    if (response.statusCode == 200){
-                        let delegation = JSON.parse(response.content).result;
-                        // console.log(delegation);
-                        delegations = delegations.concat(delegation);
-                    }
-                    else{
-                        console.log(response.statusCode);
-                    }
+                try {
+                    await fetch(url)
+                        .then(function (response) {
+                            if (response.ok) {
+                                response.json().then((data) => {
+                                    let delegation = data.result;
+                                    delegations = delegations.concat(delegation);
+                                })
+                            }
+                        });
                 }
-                catch (e){
-                    // console.log(url);
+                catch (e) {
+                    console.log(url);
                     console.log(e);
-                }    
+                }  
             }
         }
 
