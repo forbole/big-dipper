3c31f0561d2b5bb1358fe8691d1dea5bf455300f
Magic Cat <pusz.monika@gmail.com>
2021-05-20 18:06:42 +0800

    Remove unused method

--- imports/ui/proposals/Proposal.jsx
+++ imports/ui/proposals/Proposal.jsx
@@ -139,23 +139,6 @@ export default class Proposal extends Component{
         });
     }
 
-    getAddressVotingPower(address, displayPercentage, totalVotes) {
-        Meteor.call('accounts.getBalance', address, (error, result) => {
-            if (result) {
-                if (result.available && (result.available.length > 0)) {
-                    if (displayPercentage === true){
-                        return numbro(result?.available[0]?.amount / totalVotes).format('0,0.000000%');
-                    }
-                    else{
-                        return numbro(result?.available[0]?.amount / Meteor.settings.public.powerReduction).format('0,0.000000')
-
-                    }
-                    
-                }
-            }
-        })
-    }
-
     populateChartData() {
         const optionOrder = {'VOTE_OPTION_YES': 0, 'VOTE_OPTION_ABSTAIN': 1, 'VOTE_OPTION_NO': 2, 'VOTE_OPTION_NO_WITH_VETO': 3};
         let votes = this.props.proposal.votes?this.props.proposal.votes.sort(
@@ -276,11 +259,11 @@ export default class Proposal extends Component{
                         </Col>
                         <Col className="voting-power data" md={4}>
                             <i className="material-icons d-md-none">power</i>
-                            {(vote.votingPower !== undefined) ? numbro(vote.votingPower / Meteor.settings.public.powerReduction).format('0,0.000000') : ""}
+                            {(vote.votingPower!==undefined)?numbro(vote.votingPower/Meteor.settings.public.powerReduction).format('0,0.000000'):""}
                         </Col>
                         <Col className="voting-power-percent data" md={3}>
                             <i className="material-icons d-md-none">equalizer</i>
-                            {(vote.votingPower!==undefined) ? numbro(vote.votingPower/this.state.totalVotes).format('0,0.000000%') : ""}
+                            {(vote.votingPower!==undefined)?numbro(vote.votingPower/this.state.totalVotes).format('0,0.000000%'):""}
                         </Col>
                     </Row></Card>
                 )}
